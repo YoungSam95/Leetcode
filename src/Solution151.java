4893ce377bcb97ab6e81fc78a63c4e71cef25d81
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2021-03-03 00:29:38 +0800

    no message

--- src/Solution151.java
+++ src/Solution151.java
@@ -1,3 +1,10 @@
+/**
+ *151. 翻转字符串里的单词
+ * 无空格字符构成一个 单词 。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ */
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,14 +15,11 @@ public class Solution151 {
 
         String trimed = s.trim();//去掉s的首尾空格
 
-        String[] split = trimed.split("\\s+");//将s以空格分开
-
+        String[] split = trimed.split("\\s+");//将s以空格分开 \\s+为多个空格的正则表达式
         StringBuilder builder = new StringBuilder();
+
         for(int i = split.length-1;i >= 0;i--){
             String t = split[i];
-//            if(t.trim().isEmpty()){//碰到空格后continue，不写到builder中
-//                continue;
-//            }
             builder.append(t).append(" ");
         }
 
