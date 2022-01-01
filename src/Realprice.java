f9fb9ab7fe847fa8ffad293cdad91e6d60e8a827
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-01-01 23:44:05 +0800

    no message

---
+++ src/Realprice.java
@@ -0,0 +1,29 @@
+/**
+ * 问题描述：计程车价钱每位数遇到4跳过，根据现实的价钱求出真实的价钱
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Realprice {
+    public static void main(String[] args) {
+        Scanner cin=new Scanner(System.in);
+        int num=cin.nextInt();
+        int co;
+        int sum=0;
+        int h=1;
+        while (num>0)
+        {
+            co=num%10;
+            num=num/10;
+
+            if(co<4)
+                sum=sum+co*h;
+            else
+                sum=sum+(co-1)*h;
+
+            h=h*9;
+        }
+        System.out.println(sum);
+    }
+}
