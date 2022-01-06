caffd84d49e26620bc3e2217cd1614ad76b5288e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-01-06 21:16:57 +0800

    no message

--- src/Solution42.java
+++ src/Solution42.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * 42.接雨水
  *    给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
@@ -29,7 +31,7 @@ public class Solution42 {
         for(int i = 0; i < height.length; i++){
             res += Math.min(l_max[i],r_max[i]) - height[i];
         }*/
-        //解法3：双指针
+        /*//解法3：双指针
         int res = 0;
         int left = 0, right = height.length - 1;
         int l_max = 0, r_max = 0;
@@ -43,6 +45,26 @@ public class Solution42 {
                 res += r_max - height[right];
                 right--;
             }
+        }*/
+        //解法4：单调栈
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        // 遍历每个柱体
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int bottomIdx = stack.pop();
+                // 如果栈顶元素一直相等，那么全都pop出去，只留第一个。
+                while (!stack.isEmpty() && height[stack.peek()] == height[bottomIdx]) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    // stack.peek()是此次接住的雨水的左边界的位置，右边界是当前的柱体，即i。
+                    // Math.min(height[stack.peek()], height[i]) 是左右柱子高度的min，减去height[bottomIdx]就是雨水的高度。
+                    // i - stack.peek() - 1 是雨水的宽度。
+                    res += (Math.min(height[stack.peek()], height[i]) - height[bottomIdx]) * (i - stack.peek() - 1);
+                }
+            }
+            stack.push(i);
         }
         return res;
 
