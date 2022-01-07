26c8056a85db780fff7c453d1eee3bf58939c631
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-01-08 00:03:05 +0800

    no message

---
+++ src/Solution141.java
@@ -0,0 +1,46 @@
+/**
+ * 141.环形链表Ⅰ
+ *     思路：用两个指针，一个跑得快，一个跑得慢。如果不含有环，跑得快的那个指针最终会遇到 null，说明链表不含环；如果含有环，快指针最终会超慢指针一圈，和慢指针相遇，说明链表含有环。
+ */
+public class Solution141 {
+
+     private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+     }
+
+    public static boolean hasCycle(ListNode head) {
+
+         ListNode slow = head,fast = head;
+         while(fast != null && fast.next != null){
+             slow = slow.next;
+             fast = fast.next.next;
+             if(fast == slow) return true;
+         }
+         return false;
+
+    }
+
+    public static void main(String[] args){
+         ListNode node1 = new ListNode(3);
+         ListNode node2 = new ListNode(2);
+         ListNode node3 = new ListNode(0);
+         ListNode node4 = new ListNode(-4);
+         ListNode muddyNode = new ListNode(0);
+         ListNode p = node1;
+         muddyNode.next = p;
+         p.next = node2;
+         p = p.next;
+         p.next = node3;
+         p = p.next;
+         p.next = node4;
+         p = p.next;
+         p.next = node2;
+         ListNode res = muddyNode.next;
+         System.out.print(hasCycle(res));
+     }
+}
--- src/Solution148.java
+++ src/Solution148.java
@@ -1,5 +1,15 @@
 /**
  * 148.排序链表
+ * 方法一：自顶向下归并排序
+ *          找到链表的中点，以中点为分界，将链表拆分成两个子链表。寻找链表的中点可以使用快慢指针的做法，快指针每次移动 22 步，慢指针每次移动 11 步，当快指针到达链表末尾时，慢指针指向的链表节点即为链表的中点。
+ *          对两个子链表分别排序。
+ *          将两个排序后的子链表合并，得到完整的排序后的链表。可以使用「21. 合并两个有序链表」的做法，将两个有序的子链表进行合并。
+ * 方法二：自底向上归并排序
+ *          首先求得链表的长度 \textit{length}length，然后将链表拆分成子链表进行合并。
+ *          具体做法如下。
+ *            1.用subLength表示每次需要排序的子链表的长度，初始时subLength=1。
+ *            2.每次将链表拆分成若干个长度为subLength的子链表（最后一个子链表的长度可以小于subLength），按照每两个子链表一组进行合并，合并后即可得到若干个长度为subLength×2 的有序子链表（最后一个子链表的长度可以小于subLength×2）。合并两个子链表
+ *            3.subLength 的值加倍，重复第 2 步，对更长的有序子链表进行合并操作，直到有序子链表的长度大于或等于length，整个链表排序完毕。
  */
 public class Solution148 {
 
@@ -12,8 +22,110 @@ public class Solution148 {
     }
 
     public static ListNode sortList(ListNode head) {
+        //解法1：自顶向下归并排序
+        //return sortList(head,null);
+        //解法2：自底向上归并排序
+        if(head == null){
+            return head;
+        }
 
-        
+        // 1. 首先从头向后遍历,统计链表长度
+        int length = 0; // 用于统计链表长度
+        ListNode node = head;
+        while(node != null){
+            length++;
+            node = node.next;
+        }
 
+        // 2. 初始化 引入dummynode
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        // 3. 每次将链表拆分成若干个长度为subLen的子链表 , 并按照每两个子链表一组进行合并
+        for(int subLen = 1;subLen < length;subLen <<= 1){ // subLen每次左移一位（即sublen = sublen*2） PS:位运算对CPU来说效率更高
+            ListNode prev = dummyHead;
+            ListNode curr = dummyHead.next;     // curr用于记录拆分链表的位置
+
+            while(curr != null){               // 如果链表没有被拆完
+                // 3.1 拆分subLen长度的链表1
+                ListNode head_1 = curr;        // 第一个链表的头 即 curr初始的位置
+                for(int i = 1; i < subLen && curr != null && curr.next != null; i++){     // 拆分出长度为subLen的链表1
+                    curr = curr.next;
+                }
+
+                // 3.2 拆分subLen长度的链表2
+                ListNode head_2 = curr.next;  // 第二个链表的头  即 链表1尾部的下一个位置
+                curr.next = null;             // 断开第一个链表和第二个链表的链接
+                curr = head_2;                // 第二个链表头 重新赋值给curr
+                for(int i = 1;i < subLen && curr != null && curr.next != null;i++){      // 再拆分出长度为subLen的链表2
+                    curr = curr.next;
+                }
+
+                // 3.3 再次断开 第二个链表最后的next的链接
+                ListNode next = null;
+                if(curr != null){
+                    next = curr.next;   // next用于记录 拆分完两个链表的结束位置
+                    curr.next = null;   // 断开连接
+                }
+
+                // 3.4 合并两个subLen长度的有序链表
+                prev.next = merge(head_1,head_2);        // prev.next 指向排好序链表的头
+                while(prev.next != null){  // while循环 将prev移动到 subLen*2 的位置后去
+                    prev = prev.next;
+                }
+                curr = next;              // next用于记录 拆分完两个链表的结束位置
+            }
+        }
+
+        // 返回新排好序的链表
+        return dummyHead.next;
+    }
+    public static ListNode sortList(ListNode head,ListNode tail){
+        if(head == null) return head;
+        if(head.next == tail){
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while(fast != tail){
+            slow = slow.next;
+            fast = fast.next;
+            if(fast != tail) fast = fast.next;
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head,mid);
+        ListNode list2 = sortList(mid,tail);
+        ListNode res = merge(list1,list2);
+        return res;
+    }
+    public static ListNode merge(ListNode list1, ListNode list2){
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = list1, p2 = list2;
+        while (p1 != null && p2 != null) {
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的的节点接到 p 指针
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+        if (p1 != null) p.next = p1;
+        if (p2 != null) p.next = p2;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args){
+        ListNode head = new ListNode(3,new ListNode(1,new ListNode(6,new ListNode(5))));
+        ListNode res = sortList(head);
+        while(res != null){
+            System.out.println(res.val);
+            res = res.next;
+        }
     }
 }
