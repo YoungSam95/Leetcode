20848f6aad87feca0b3b82d66d27fe47dd48b3d0
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-01-07 17:12:42 +0800

    no message

---
+++ src/Solution148.java
@@ -0,0 +1,19 @@
+/**
+ * 148.排序链表
+ */
+public class Solution148 {
+
+    public static class ListNode { int val;
+
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode sortList(ListNode head) {
+
+        
+
+    }
+}
