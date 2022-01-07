92b06aa8e5f66dfc8525f36c3eb188cdff48e111
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-01-07 16:48:51 +0800

    no message

---
+++ src/Solution2.java
@@ -0,0 +1,55 @@
+/**
+ * 2.两数相加
+ */
+public class Solution2 {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        //两条链表上的指针
+        ListNode p1 = l1, p2 = l2;
+        //虚拟头节点
+        ListNode dummy = new ListNode(-1);
+        //结果
+        ListNode p = dummy;
+        //进位
+        int carry = 0;
+        while(p1 != null || p2 != null || carry > 0){
+            int val = carry;//先加进位
+            if(p1 != null){
+                val += p1.val;
+                p1 = p1.next;
+            }
+            if(p2 != null){
+                val += p2.val;
+                p2 = p2.next;
+            }
+            //处理进位
+            carry = val/10;
+            val = val%10;
+            //构建新节点
+            p.next = new ListNode(val);
+            p = p.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args){
+
+        ListNode a = new ListNode(2,new ListNode(4,new ListNode(3)));
+        ListNode b = new ListNode(5,new ListNode(6,new ListNode(4)));
+        ListNode res = addTwoNumbers(a,b);
+        while(res != null){
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+}
--- src/Solution206.java
+++ src/Solution206.java
@@ -14,19 +14,31 @@ public class Solution206 {
 
     public static ListNode reverseList(ListNode head) {
 
-        if (head == null || head.next == null) {
+        //方法1：递归
+        /*if (head == null || head.next == null) {//head为null或者递归到最后一个元素就返回
             return head;
         }
-        ListNode last = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return last;
+        ListNode last = reverseList(head.next);//last接受上层栈最后一个元素；
+        head.next.next = head;//把head放在last后面
+        head.next = null;//把head的last指向null；
+        return last;*/
+
+        //方法2：迭代
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 
     public static void main(String[] args){
 
-        ListNode list = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5)))));
-        ListNode res = reverseList(list);
+        ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5)))));
+        ListNode res = reverseList(head);
         while(res != null){
             System.out.println(res.val);
             res = res.next;
