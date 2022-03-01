ee1870c5815560f5e559a87b1604148b1ad77e86
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-03-01 19:25:53 +0800

    652. 寻找重复的子树

---
+++ src/leetcode/Solution652.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import common.TreeNode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 652. 寻找重复的子树
+ */
+
+public class Solution652 {
+
+    //记录所有子树以及出现的次数
+    private HashMap<String,Integer> memo = new HashMap<>();
+    //记录重复的子树的根节点
+    private LinkedList<TreeNode> res = new LinkedList<>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+
+    public String traverse(TreeNode root){
+        if(root == null) return "#";//#代表null
+
+        String left = traverse(root.left);
+        String right = traverse(root.right);
+        String subTree = left + "," + right + "," + root.val;
+
+        int freq = memo.getOrDefault(subTree,0);
+        if(freq == 1){
+            res.add(root);
+        }
+        memo.put(subTree,freq+1);
+        return subTree;
+    }
+}
