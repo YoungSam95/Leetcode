460c6b274b4a32aa82b96a1a8ab9413eb200d398
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-03-31 17:34:51 +0800

    为TreeNode类提供反序列化方法

--- src/common/TreeNode.java
+++ src/common/TreeNode.java
@@ -1,5 +1,8 @@
 package common;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -10,5 +13,30 @@ public class TreeNode {
       this.val = val;
       this.left = left;
       this.right = right;
+    }
+    public TreeNode deserialize(String[] nodes){
+        if(nodes.length == 0) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        for(int i = 1; i < nodes.length;){
+            //队列中存的都是父节点
+            TreeNode parent = q.poll();
+            //左节点
+            String left = nodes[i++];
+            if(!left.equals("null")){
+                parent.left = new TreeNode(Integer.parseInt(left));
+            }else{
+                parent.left = null;
+            }
+            //右节点
+            String right = nodes[i++];
+            if(!right.equals("null")){
+                parent.right = new TreeNode(Integer.parseInt(right));
+            }else{
+                parent.right = null;
+            }
+        }
+        return root;
     }
 }
