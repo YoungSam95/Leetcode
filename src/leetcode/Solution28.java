44634f1f504308ca739d592cb504052420158349
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-04-02 20:03:37 +0800

    28.实现strStr

---
+++ src/leetcode/Solution28.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * 28.实现strStr()
+ */
+public class Solution28 {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+    public static void main(String[] args){
+        System.out.println("hello".indexOf("b"));
+    }
+}
