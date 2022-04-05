1c3845756123e1059eda0a7d3720ba127c363f36
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-04-06 02:03:31 +0800

    100.相同的树

---
+++ src/leetcode/Solution100.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import common.TreeNode;
+
+/**
+ * 100.相同的树
+ */
+public class Solution100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
