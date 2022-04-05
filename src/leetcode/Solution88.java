9d4201b93024bcc1e2a8b849ef60c48c1f064874
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-04-05 21:07:16 +0800

    88.合并两个有序数组

---
+++ src/leetcode/Solution88.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 88.合并两个有序数组
+ */
+public class Solution88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int tail = nums1.length - 1;
+        m--;
+        n--;
+        while(n >= 0){
+            if(m < 0 || nums1[m] <= nums2[n]){
+                nums1[tail--] = nums2[n--];
+            }else{
+                nums1[tail--] = nums1[m--];
+            }
+        }
+    }
+}
