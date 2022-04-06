5a1bc1ab7493a4b6981fc8c59acd69e418ef4993
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-04-07 03:02:28 +0800

    796.旋转字符串优化

--- src/leetcode/Solution796.java
+++ src/leetcode/Solution796.java
@@ -6,15 +6,16 @@ package leetcode;
 
 public class Solution796 {
     public static boolean rotateString(String s, String goal) {
-        for(int i = 0; i < s.length() - 1; i++){
+        /*for(int i = 0; i < s.length() - 1; i++){
             s = s.substring(1,s.length()) + s.charAt(0);
             if(s.equals(goal)) return true;
         }
-        return false;
+        return false;*/
+        return s.length() == goal.length() && (s+s).contains(goal);
     }
     public static void main(String[] args){
-        String s = "abcde";
-        String goal = "cdeab";
+        String s = "bbbacddceeb";
+        String goal = "ceebbbbacdd";
         System.out.println(rotateString(s,goal));
     }
 }
