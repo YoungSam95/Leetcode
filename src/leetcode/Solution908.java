0f903ff68e8a2b153c9ad6321e70c8f4f5e1393d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-04-30 01:48:43 +0800

    908.最小差值Ⅰ

---
+++ src/leetcode/Solution908.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * 908.最小差值Ⅰ
+ * 思路：看最值可否足够接近，可以就是0，不可以就是最小差值
+ */
+public class Solution908 {
+    public int smallestRangeI(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(max,nums[i]);
+            min = Math.min(min,nums[i]);
+        }
+        return Math.max(0,max - min - 2 * k);
+    }
+}
