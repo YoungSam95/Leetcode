33acd4da5980f176f8991d4ad153a8a6adfca1bf
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-05-04 22:51:44 +0800

    1823队列版

--- src/leetcode/Solution1823.java
+++ src/leetcode/Solution1823.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * 1823. 找出游戏的获胜者
  */
@@ -17,7 +20,8 @@ public class Solution1823 {
         }
     }
     public int findTheWinner(int n, int k) {
-        if(k == 1) return n;
+        /**链表模拟**/
+        /*if(k == 1) return n;
         Node curr = new Node(1);
         Node head = curr;
         for(int i = 2; i <= n; i++){
@@ -34,6 +38,18 @@ public class Solution1823 {
             curr.next = curr.next.next;
             curr = curr.next;
         }
-        return curr.number;
+        return curr.number;*/
+        /**队列模拟**/
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i = 1; i <= n; i++){
+            queue.offer(i);
+        }
+        while(queue.size() > 1){
+            for(int i = 1; i < k; i++){
+                queue.offer(queue.poll());
+            }
+            queue.poll();
+        }
+        return queue.peek();
     }
 }
