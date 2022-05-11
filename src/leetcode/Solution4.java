a157b2feca6084fc7b266aad155b356b4ed00286
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-05-11 23:18:03 +0800

    449. 序列化和反序列化二叉搜索树

---
+++ src/leetcode/Solution4.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * 4. 寻找两个正序数组的中位数
+ */
+public class Solution4 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return -1;
+    }
+}
--- src/leetcode/Solution449.java
+++ src/leetcode/Solution449.java
@@ -1,21 +1,16 @@
-package common;
+package leetcode;
 
-import java.util.ArrayDeque;
+import common.TreeNode;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class TreeNode {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    public TreeNode() {}
-    public TreeNode(int val) { this.val = val; }
-    public TreeNode(int val, TreeNode left, TreeNode right) {
-      this.val = val;
-      this.left = left;
-      this.right = right;
-    }
-    public String serialize(TreeNode root) {
+/**
+ * 449. 序列化和反序列化二叉搜索树
+ */
+public class Solution449 {
+    // Encodes a tree to a single string.
+    public static String serialize(TreeNode root) {
         if (root == null) return "";
         StringBuilder sb = new StringBuilder();
         // 初始化队列，将 root 加入队列
@@ -38,31 +33,43 @@ public class TreeNode {
 
         return sb.toString();
     }
-    public TreeNode deserialize(String[] nodes){
-        if(nodes.length == 0) return null;
+
+    // Decodes your encoded data to tree.
+    public static TreeNode deserialize(String data) {
+        if (data.isEmpty()) return null;
+        String[] nodes = data.split(",");
+        // 第一个元素就是 root 的值
         TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+
+        // 队列 q 记录父节点，将 root 加入队列
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        for(int i = 1; i < nodes.length;){
-            //队列中存的都是父节点
+
+        for (int i = 1; i < nodes.length; ) {
+            // 队列中存的都是父节点
             TreeNode parent = q.poll();
-            //左节点
+            // 父节点对应的左侧子节点的值
             String left = nodes[i++];
-            if(!left.equals("null")){
+            if (!left.equals("null")) {
                 parent.left = new TreeNode(Integer.parseInt(left));
                 q.offer(parent.left);
-            }else{
+            } else {
                 parent.left = null;
             }
-            //右节点
+            // 父节点对应的右侧子节点的值
             String right = nodes[i++];
-            if(!right.equals("null")){
+            if (!right.equals("null")) {
                 parent.right = new TreeNode(Integer.parseInt(right));
                 q.offer(parent.right);
-            }else{
+            } else {
                 parent.right = null;
             }
         }
         return root;
+    }
+    public static void main(String[] args){
+        String data = "1,2,3,null,null,4,5";
+        TreeNode root = deserialize(data);
+        TreeNode root2 = deserialize(serialize(root));
     }
 }
