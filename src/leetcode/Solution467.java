0a60497b845f8b53ba2223bb8a4aa12097fac7be
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-05-25 17:17:59 +0800

    467. 环绕字符串中唯一的子字符串

---
+++ src/leetcode/Solution467.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 467. 环绕字符串中唯一的子字符串
+ */
+public class Solution467 {
+    public static int findSubstringInWraproundString(String p) {
+        int[] dp = new int[26];
+        int k = 0;
+        for (int i = 0; i < p.length(); ++i) {
+            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) + 26) % 26 == 1) { // 字符之差为 1 或 -25
+                ++k;
+            } else {
+                k = 1;
+            }
+            dp[p.charAt(i) - 'a'] = Math.max(dp[p.charAt(i) - 'a'], k);
+        }
+        return Arrays.stream(dp).sum();
+
+    }
+    public static void main(String[] args){
+        String p = "cac";
+        System.out.println(findSubstringInWraproundString(p));
+    }
+}
