f6a422422198daf401ad68e6f914aa8c64960c26
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-06-14 01:47:51 +0800

    498. 对角线遍历

---
+++ src/leetcode/Solution498.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+/**
+ * 498. 对角线遍历
+ */
+public class Solution498 {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int x = 0, y = 0, index = 0;
+        boolean flag = true;
+        int[] res = new int[m * n];
+        while(index < m * n){
+            res[index++] = mat[x][y];
+            if(flag){//向斜上
+                if(x - 1 >= 0){
+                    if(y + 1 < n){
+                        x--;
+                        y++;
+                    }else{
+                        x++;
+                        flag = false;
+                    }
+                }else{
+                    if(y + 1 < n){
+                        y++;
+                        flag = false;
+                    }else{
+                        x++;
+                        flag = false;
+                    }
+                }
+            }else{//向斜下
+                if(x + 1 < m){
+                    if(y - 1 >= 0){
+                        x++;
+                        y--;
+                    }else{
+                        x++;
+                        flag = true;
+                    }
+                }else{
+                   y++;
+                   flag = true;
+                }
+            }
+        }
+        return res;
+    }
+}
