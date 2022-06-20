9142bdc18f18c09ec981b7eb10c710c9c9f77adb
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-06-21 00:20:22 +0800

    1108. IP 地址无效化

---
+++ src/leetcode/Solution1108.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * 1108. IP 地址无效化
+ */
+public class Solution1108 {
+    public String defangIPaddr(String address) {
+        return address.replace(".","[.]");
+    }
+}
