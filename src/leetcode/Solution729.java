33d1b83b21dfcab0e56c1c87e9a9c568a05b8c07
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-07-05 13:38:12 +0800

    729. 我的日程安排表 I

---
+++ src/leetcode/Solution729.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 729. 我的日程安排表 I
+ */
+public class Solution729 {
+    class MyCalendar {
+        private List<int[]> booked;
+
+        public MyCalendar() {
+            booked = new ArrayList<int[]>();
+        }
+
+        public boolean book(int start, int end) {
+            for (int[] arr : booked) {
+                int l = arr[0], r = arr[1];
+                if (l < end && start < r) {
+                    return false;
+                }
+            }
+            booked.add(new int[]{start, end});
+            return true;
+        }
+    }
+}
