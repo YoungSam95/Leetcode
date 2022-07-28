9e00ed12928ba73d1ea92e8cf8a4f4e31c1b73e7
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-07-28 14:21:47 +0800

    1331. 数组序号转换

---
+++ src/leetcode/Solution1331.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *1331. 数组序号转换
+ */
+public class Solution1331 {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] clone = arr.clone();
+        Arrays.sort(clone);
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = arr.length, idx = 0;
+        for (int i : clone) {
+            if (!map.containsKey(i)) map.put(i, ++idx);
+        }
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) ans[i] = map.get(arr[i]);
+        return ans;
+    }
+}
