c5bc26a12cb0c5ec77c4f14e662cd027c5db1236
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-08-16 02:04:21 +0800

    1656. 设计有序流

---
+++ src/leetcode/Solution1656.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1656. 设计有序流
+ */
+public class Solution1656 {
+    class OrderedStream {
+
+        private String[] values;
+        private int ptr;
+
+        public OrderedStream(int n) {
+            values = new String[n + 1];
+            ptr = 1;
+        }
+
+        public List<String> insert(int idKey, String value) {
+            values[idKey] = value;
+            List<String> list = new ArrayList<>();
+            if(idKey == ptr) {
+                for (; ptr < values.length; ptr++) {
+                    if (values[ptr] != null) {
+                        list.add(values[ptr]);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
