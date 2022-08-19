d31a87c5d450315516756e332cb18e7419e68d92
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-08-20 01:11:32 +0800

    119. 杨辉三角 II

---
+++ src/leetcode/Solution119.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 119. 杨辉三角 II
+ */
+public class Solution119 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        res.add(1);//每一行第一个数都是1;
+        if(rowIndex == 0) return res;
+        List<Integer> pre = new ArrayList<>();
+        pre = getRow(rowIndex - 1);//递归计算上一行
+        for(int i = 0; i < pre.size() - 1; i++){
+            res.add(pre.get(i) + pre.get(i + 1));
+        }
+        res.add(1);//每一行最后一个数都是1;
+        return res;
+    }
+}
