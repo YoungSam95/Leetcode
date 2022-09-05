dab12edd6e26d366447694fce0d652d5e52aa87e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-09-06 01:54:14 +0800

    828. 统计子串中的唯一字符

--- src/leetcode/Solution828.java
+++ src/leetcode/Solution828.java
@@ -1,13 +1,15 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * 828. 统计子串中的唯一字符
  */
 public class Solution828 {
-    public int uniqueLetterString(String s) {
+    public int uniqueLetterString1(String s) {
         Map<String,Integer> map = new HashMap<>();
         int res = 0;
         for(int i = 0; i < s.length(); i++){
@@ -38,8 +40,29 @@ public class Solution828 {
         }
         return count;
     }
+
+    public int uniqueLetterString(String s) {
+        Map<Character, List<Integer>> index = new HashMap<Character, List<Integer>>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!index.containsKey(c)) {
+                index.put(c, new ArrayList<Integer>());
+                index.get(c).add(-1);
+            }
+            index.get(c).add(i);
+        }
+        int res = 0;
+        for (Map.Entry<Character, List<Integer>> entry : index.entrySet()) {
+            List<Integer> arr = entry.getValue();
+            arr.add(s.length());
+            for (int i = 1; i < arr.size() - 1; i++) {
+                res += (arr.get(i) - arr.get(i - 1)) * (arr.get(i + 1) - arr.get(i));
+            }
+        }
+        return res;
+    }
     public static void main(String[] args){
-        String s = "LEETCODE";
+        String s = "ABC";
         System.out.println(new Solution828().uniqueLetterString(s));
     }
 }
