de5c01adfac30e1c3be276b5e2613f25c077f7b8
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-09-21 21:11:32 +0800

    827添加注释

--- src/leetcode/Solution827.java
+++ src/leetcode/Solution827.java
@@ -9,17 +9,17 @@ import java.util.Set;
  * 827. 最大人工岛
  */
 public class Solution827 {
-    static int[] d = {0, -1, 0, 1, 0};
+    static int[] d = {0, -1, 0, 1, 0};//上下左右四个方向
 
     public int largestIsland(int[][] grid) {
         int n = grid.length, res = 0;
-        int[][] tag = new int[n][n];
-        Map<Integer, Integer> area = new HashMap<>();
+        int[][] tag = new int[n][n];//为岛屿做标记
+        Map<Integer, Integer> area = new HashMap<>();//记录岛屿面积
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && tag[i][j] == 0) {
-                    int t = i * n + j + 1;
-                    area.put(t, dfs(grid, i, j, tag, t));
+                if (grid[i][j] == 1 && tag[i][j] == 0) {//如果是陆地并且还没划分到某个岛屿
+                    int t = i * n + j + 1;//岛屿编号
+                    area.put(t, dfs(grid, i, j, tag, t));//深度搜多t编号岛屿的面积
                     res = Math.max(res, area.get(t));
                 }
             }
@@ -27,15 +27,15 @@ public class Solution827 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
-                    int z = 1;
-                    Set<Integer> connected = new HashSet<>();
-                    for (int k = 0; k < 4; k++) {
+                    int z = 1;//将当前海变成陆地，面积即为1
+                    Set<Integer> connected = new HashSet<>();//用于排除重复的岛屿
+                    for (int k = 0; k < 4; k++) {//向上下左右四个方向搜索
                         int x = i + d[k], y = j + d[k + 1];
                         if (!valid(n, x, y) || tag[x][y] == 0 || connected.contains(tag[x][y])) {
-                            continue;
+                            continue;//超出边界或者不属于岛屿或者是搜索过的岛屿，跳过
                         }
-                        z += area.get(tag[x][y]);
-                        connected.add(tag[x][y]);
+                        z += area.get(tag[x][y]);//面积增加
+                        connected.add(tag[x][y]);//记录
                     }
                     res = Math.max(res, z);
                 }
