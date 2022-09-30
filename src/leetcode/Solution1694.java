61b35af6bbc687306dce8078bd93b4162509af93
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-10-01 02:02:09 +0800

    1694. 重新格式化电话号码

---
+++ src/leetcode/Solution1694.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * 1694. 重新格式化电话号码
+ */
+public class Solution1694 {
+    public String reformatNumber(String number) {
+        StringBuilder digits = new StringBuilder();
+        for (int i = 0; i < number.length(); ++i) {
+            char ch = number.charAt(i);
+            if (Character.isDigit(ch)) {
+                digits.append(ch);
+            }
+        }
+
+        int n = digits.length();
+        int pt = 0;
+        StringBuilder ans = new StringBuilder();
+        while (n > 0) {
+            if (n > 4) {
+                ans.append(digits.substring(pt, pt + 3) + "-");
+                pt += 3;
+                n -= 3;
+            } else {
+                if (n == 4) {
+                    ans.append(digits.substring(pt, pt + 2) + "-" + digits.substring(pt + 2, pt + 4));
+                } else {
+                    ans.append(digits.substring(pt, pt + n));
+                }
+                break;
+            }
+        }
+        return ans.toString();
+    }
+}
