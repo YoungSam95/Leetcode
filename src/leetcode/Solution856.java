2354cd723d8bfc8cc5d23ea7779159ef498967fa
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-10-09 01:41:50 +0800

    856. 括号的分数

---
+++ src/leetcode/Solution856.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 856. 括号的分数
+ */
+public class Solution856 {
+    public int scoreOfParentheses(String s) {
+        Deque<Integer> st = new ArrayDeque<Integer>();
+        st.push(0);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push(0);
+            } else {
+                int v = st.pop();
+                int top = st.pop() + Math.max(2 * v, 1);
+                st.push(top);
+            }
+        }
+        return st.peek();
+    }
+}
