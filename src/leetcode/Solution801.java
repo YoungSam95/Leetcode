46911130be32f80f6f3968db550378ee9ec1308f
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-10-10 11:24:57 +0800

    801. 使序列递增的最小交换次数

---
+++ src/leetcode/Solution801.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 801. 使序列递增的最小交换次数
+ */
+public class Solution801 {
+    public int minSwap(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int[][] f = new int[n][2];
+        for (int i = 1; i < n; i++) f[i][0] = f[i][1] = n + 10;
+        f[0][1] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
+                f[i][0] = f[i - 1][0];
+                f[i][1] = f[i - 1][1] + 1;
+            }
+            if (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1]) {
+                f[i][0] = Math.min(f[i][0], f[i - 1][1]);
+                f[i][1] = Math.min(f[i][1], f[i - 1][0] + 1);
+            }
+        }
+        return Math.min(f[n - 1][0], f[n - 1][1]);
+    }
+}
