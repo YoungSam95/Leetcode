4f7343e4f16036d84a69d5a4cc7dc8373b7dbedf
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-10-14 00:18:43 +0800

    940. 不同的子序列 II

---
+++ src/leetcode/Solution940.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 940. 不同的子序列 II
+ */
+public class Solution940 {
+    public int distinctSubseqII(String s) {
+        final int MOD = 1000000007;
+        int[] last = new int[26];
+        Arrays.fill(last, -1);
+
+        int n = s.length();
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                if (last[j] != -1) {
+                    f[i] = (f[i] + f[last[j]]) % MOD;
+                }
+            }
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (last[i] != -1) {
+                ans = (ans + f[last[i]]) % MOD;
+            }
+        }
+        return ans;
+    }
+}
