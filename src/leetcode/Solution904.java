6dca664956a4f53e418dc7469a8fb70bdbd36eef
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-10-17 02:18:19 +0800

    904. 水果成篮

---
+++ src/leetcode/Solution904.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 904. 水果成篮
+ */
+public class Solution904 {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        int left = 0, res = 0;
+        for(int right = 0; right < n; ++right) {
+            map.put(fruits[right], map.getOrDefault(fruits[right],0) + 1);
+            while(map.size() > 2){
+                map.put(fruits[left],map.get(fruits[left]) - 1);
+                if(map.get(fruits[left]) == 0){
+                    map.remove(fruits[left]);
+                }
+                ++left;
+            }
+            res = Math.max(res,right - left + 1);
+        }
+        return res;
+    }
+}
