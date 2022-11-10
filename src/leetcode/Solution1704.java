7e533a269ff5de2a127deaccf548b47cff63f2ab
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-11-11 00:08:17 +0800

    1704. 判断字符串的两半是否相似

---
+++ src/leetcode/Solution1704.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 1704. 判断字符串的两半是否相似
+ */
+public class Solution1704 {
+    public boolean halvesAreAlike(String s) {
+        String a = s.substring(0, s.length() / 2);
+        String b = s.substring(s.length() / 2);
+        String h = "aeiouAEIOU";
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (h.indexOf(a.charAt(i)) >= 0) {
+                sum1++;
+            }
+        }
+        for (int i = 0; i < b.length(); i++) {
+            if (h.indexOf(b.charAt(i)) >= 0) {
+                sum2++;
+            }
+        }
+        return sum1 == sum2;
+    }
+}
