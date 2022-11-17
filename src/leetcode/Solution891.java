53b794e46a50cc3a334ed2a253565e75ec52801a
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-11-18 02:37:51 +0800

    891. 子序列宽度之和

---
+++ src/leetcode/Solution891.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 891. 子序列宽度之和
+ */
+public class Solution891 {
+    public int sumSubseqWidths(int[] nums) {
+        final int MOD = 1000000007;
+        Arrays.sort(nums);
+        long res = 0;
+        long x = nums[0], y = 2;
+        for (int j = 1; j < nums.length; j++) {
+            res = (res + nums[j] * (y - 1) - x) % MOD;
+            x = (x * 2 + nums[j]) % MOD;
+            y = y * 2 % MOD;
+        }
+        return (int) ((res + MOD) % MOD);
+    }
+}
