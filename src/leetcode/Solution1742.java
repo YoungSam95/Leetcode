e86ad2f37d2d937b9cfcce3c637dd7f80950db7b
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-11-23 00:12:33 +0800

    1742. 盒子中小球的最大数量

---
+++ src/leetcode/Solution1742.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1742. 盒子中小球的最大数量
+ */
+public class Solution1742 {
+    public int countBalls(int lowLimit, int highLimit) {
+        Map<Integer,Integer> count = new HashMap<>();
+        int res = 0;
+        for(int i = lowLimit; i <= highLimit; i++){
+            int index = 0;
+            int ball = i;
+            while(ball > 0){
+                index += ball % 10;
+                ball /= 10;
+            }
+            count.put(index,count.getOrDefault(index,0) + 1);
+            res = Math.max(res,count.get(index));
+        }
+        return res;
+    }
+}
