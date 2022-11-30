21015ee1beeacd8cc5823ae0ef58e7d2993e54de
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-12-01 00:14:38 +0800

    守护线程

--- src/serialize/MyThread.java
+++ src/serialize/MyThread.java
@@ -10,7 +10,9 @@ public class MyThread implements Runnable{
         MyThread myThread = new MyThread();
         Thread thread1 = new Thread(myThread,"线程1");
         Thread thread2 = new Thread(myThread,"线程2");
-        thread2.setDaemon(true);
+        //thread2.setDaemon(true);
+        thread1.setPriority(5);
+        thread2.setPriority(10);
         thread1.start();
         thread2.start();
         System.out.println(Thread.currentThread().getName());
