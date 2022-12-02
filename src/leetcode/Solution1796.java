3d66c3576dcb833ff22fa58fc217cb39b6246670
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-12-03 00:29:36 +0800

    1796. 字符串中第二大的数字

---
+++ src/leetcode/Solution1796.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1796. 字符串中第二大的数字
+ */
+public class Solution1796 {
+    public int secondHighest(String s) {
+        int first = -1, second = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                if (num > first) {
+                    second = first;
+                    first = num;
+                } else if (num < first && num > second) {
+                    second = num;
+                }
+            }
+        }
+        return second;
+    }
+    public static void main(String[] args) {
+        String s = "dfa12321afd";
+        System.out.println(new Solution1796().secondHighest(s));
+    }
+}
