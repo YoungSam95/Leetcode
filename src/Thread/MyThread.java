d0b216d5dae765d506d0f44515bc5e1364e4dce6
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-12-08 00:11:26 +0800

    1812. 判断国际象棋棋盘中一个格子的颜色

---
+++ src/Thread/MyThread.java
@@ -0,0 +1,20 @@
+package Thread;
+
+public class MyThread implements Runnable{
+    @Override
+    public void run() {
+        //打印当前线程名字
+        System.out.println(Thread.currentThread().getName());
+    }
+    public static void main(String[] args){
+        MyThread myThread = new MyThread();
+        Thread thread1 = new Thread(myThread,"线程1");
+        Thread thread2 = new Thread(myThread,"线程2");
+        //thread2.setDaemon(true);
+        thread1.setPriority(5);
+        thread2.setPriority(10);
+        thread1.start();
+        thread2.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
---
+++ src/leetcode/Solution1812.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * 1812. 判断国际象棋棋盘中一个格子的颜色
+ */
+public class Solution1812 {
+    public boolean squareIsWhite(String coordinates) {
+        return ((coordinates.charAt(0) - 'a') + (coordinates.charAt(1) - '0')) % 2 == 0;
+    }
+}
