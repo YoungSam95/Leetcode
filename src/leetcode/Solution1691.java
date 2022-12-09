8324d3ba2357c5a3bc8f75ceb9b7614601b514cc
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-12-10 00:09:38 +0800

    1691. 堆叠长方体的最大高度

---
+++ src/leetcode/Solution1691.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 1691. 堆叠长方体的最大高度
+ */
+public class Solution1691 {
+    public int maxHeight(int[][] cuboids) {
+        int n = cuboids.length;
+        for (int[] v : cuboids) {
+            Arrays.sort(v);
+        }
+        Arrays.sort(cuboids, (a, b) -> (a[0] + a[1] + a[2]) - (b[0] + b[1] + b[2]));
+        int ans = 0;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = cuboids[i][2];
+            for (int j = 0; j < i; j++) {
+                if (cuboids[i][0] >= cuboids[j][0] &&
+                        cuboids[i][1] >= cuboids[j][1] &&
+                        cuboids[i][2] >= cuboids[j][2]) {
+                    dp[i] = Math.max(dp[i], dp[j] + cuboids[i][2]);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
