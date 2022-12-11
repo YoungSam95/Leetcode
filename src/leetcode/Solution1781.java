e40526216aa38907fdefd03ec9f97fa358e5b0ed
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-12-12 00:07:54 +0800

    1781. 所有子字符串美丽值之和

---
+++ src/leetcode/Solution1781.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * 1781. 所有子字符串美丽值之和
+ */
+public class Solution1781 {
+    public int beautySum(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            int[] count = new int[26];
+            for (int j = i; j < s.length(); ++j) {
+                char ch = s.charAt(j);
+                count[ch - 'a'] += 1;
+                int max_value = Integer.MIN_VALUE;
+                int min_value = Integer.MAX_VALUE;
+                for (Integer value: count) {
+                    if (value > 0 && value > max_value) {
+                        max_value = value;
+                    }
+                    if (value > 0 && value < min_value) {
+                        min_value = value;
+                    }
+                }
+                ans += (max_value - min_value);
+            }
+        }
+        return ans;
+    }
+}
