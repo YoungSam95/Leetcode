09722b8cc9ffa824275e81f507407b419d960df2
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-12-24 00:29:06 +0800

    1754. 构造字典序最大的合并字符串

---
+++ src/leetcode/Solution1754.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * 1754. 构造字典序最大的合并字符串
+ */
+public class Solution1754 {
+
+    public String largestMerge(String word1, String word2) {
+        StringBuilder merge = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length() && word1.substring(i).compareTo(word2.substring(j)) > 0) {
+                merge.append(word1.charAt(i));
+                i++;
+            } else {
+                merge.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return merge.toString();
+    }
+}
