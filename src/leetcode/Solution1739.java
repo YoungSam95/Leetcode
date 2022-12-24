34849b6a518332f8000509f431aa0a6833d2e1b6
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-12-25 00:16:11 +0800

    1739. 放置盒子

---
+++ src/leetcode/Solution1739.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * 1739. 放置盒子
+ */
+public class Solution1739 {
+    public int minimumBoxes(int n) {
+        int cur = 1, i = 1, j = 1;
+        while (n > cur) {
+            n -= cur;
+            i++;
+            cur += i;
+        }
+        cur = 1;
+        while (n > cur) {
+            n -= cur;
+            j++;
+            cur++;
+        }
+        return (i - 1) * i / 2 + j;
+    }
+}
