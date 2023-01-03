43e7da7953b92aebb145383b0cfc41f3fcf5478b
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-01-04 00:35:10 +0800

    no message

--- src/leetcode/Solution2042.java
+++ src/leetcode/Solution2042.java
@@ -18,5 +18,23 @@ public class Solution2042 {
         }
         return true;
     }
-
+    public boolean areNumbersAscending2(String s) {
+        int pre = 0, pos = 0;
+        while (pos < s.length()) {
+            if (Character.isDigit(s.charAt(pos))) {
+                int cur = 0;
+                while (pos < s.length() && Character.isDigit(s.charAt(pos))) {
+                    cur = cur * 10 + s.charAt(pos) - '0';
+                    pos++;
+                }
+                if (cur <= pre) {
+                    return false;
+                }
+                pre = cur;
+            } else {
+                pos++;
+            }
+        }
+        return true;
+    }
 }
