87f98704a6519abc47d40695ee06823a151b170e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-01-15 00:14:23 +0800

    2293. 极大极小游戏

---
+++ src/leetcode/Solution2293.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 2293. 极大极小游戏
+ */
+public class Solution2293 {
+    public int minMaxGame(int[] nums) {
+        int n = nums.length;
+        while (n != 1) {
+            int[] newNums = new int[n / 2];
+            for (int i = 0; i < newNums.length; i++) {
+                if (i % 2 == 0) {
+                    newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                } else {
+                    newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                }
+            }
+            nums = newNums;
+            n /= 2;
+        }
+        return nums[0];
+    }
+}
