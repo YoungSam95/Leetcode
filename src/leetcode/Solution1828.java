1f14b1c16dd64d2fe4154c901ffd373e8ad93c05
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-01-24 10:21:01 +0800

    1828. 统计一个圆中点的数目

---
+++ src/leetcode/Solution1828.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 1828. 统计一个圆中点的数目
+ */
+public class Solution1828 {
+    public int[] countPoints(int[][] points, int[][] queries) {
+        int m = points.length, n = queries.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int cx = queries[i][0], cy = queries[i][1], cr = queries[i][2];
+            for (int j = 0; j < m; ++j) {
+                int px = points[j][0], py = points[j][1];
+                if ((cx - px) * (cx - px) + (cy - py) * (cy - py) <= cr * cr) {
+                    ++ans[i];
+                }
+            }
+        }
+        return ans;
+    }
+}
