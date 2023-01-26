08128c0eeefee61ec2ac3fd55c45f9af0d3d5a3a
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-01-26 10:51:33 +0800

    1663. 具有给定数值的最小字符串

---
+++ src/leetcode/Solution1663.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 1663. 具有给定数值的最小字符串
+ */
+public class Solution1663 {
+    public String getSmallestString(int n, int k) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            int lower = Math.max(1, k - (n - i) * 26);
+            k -= lower;
+            ans.append((char) ('a' + lower - 1));
+        }
+        return ans.toString();
+    }
+}
