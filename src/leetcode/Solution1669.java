1531a1dcd591444cd466798d0ba3d07942584510
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-01-30 12:27:53 +0800

    1669. 合并两个链表

---
+++ src/leetcode/Solution1669.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import common.ListNode;
+
+/**
+ * 1669. 合并两个链表
+ */
+public class Solution1669 {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode preA = list1;
+        for (int i = 0; i < a - 1; i++) {
+            preA = preA.next;
+        }
+        ListNode preB = preA;
+        for (int i = 0; i < b - a + 2; i++) {
+            preB = preB.next;
+        }
+        preA.next = list2;
+        while (list2.next != null) {
+            list2 = list2.next;
+        }
+        list2.next = preB;
+        return list1;
+    }
+}
