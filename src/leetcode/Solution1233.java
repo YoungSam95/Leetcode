ba1bb8938c6381b088c62dba1df7b0ed36fe66fe
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-02-08 00:41:00 +0800

    1233. 删除子文件夹

---
+++ src/leetcode/Solution1233.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 1233. 删除子文件夹
+ */
+public class Solution1233 {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> ans = new ArrayList<String>();
+        ans.add(folder[0]);
+        for (int i = 1; i < folder.length; ++i) {
+            int pre = ans.get(ans.size() - 1).length();
+            if (!(pre < folder[i].length() && ans.get(ans.size() - 1).equals(folder[i].substring(0, pre)) && folder[i].charAt(pre) == '/')) {
+                ans.add(folder[i]);
+            }
+        }
+        return ans;
+    }
+}
