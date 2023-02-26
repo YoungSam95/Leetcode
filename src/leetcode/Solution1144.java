358028bd355493d797d2fc068d2ee4ec47fcbefd
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-02-27 00:10:08 +0800

    1144. 递减元素使数组呈锯齿状

---
+++ src/leetcode/Solution1144.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * 1144. 递减元素使数组呈锯齿状
+ */
+public class Solution1144 {
+    public int movesToMakeZigzag(int[] nums) {
+        return Math.min(help(nums, 0), help(nums, 1));
+    }
+
+    public int help(int[] nums, int pos) {
+        int res = 0;
+        for (int i = pos; i < nums.length; i += 2) {
+            int a = 0;
+            if (i - 1 >= 0) {
+                a = Math.max(a, nums[i] - nums[i - 1] + 1);
+            }
+            if (i + 1 < nums.length) {
+                a = Math.max(a, nums[i] - nums[i + 1] + 1);
+            }
+            res += a;
+        }
+        return res;
+    }
+}
