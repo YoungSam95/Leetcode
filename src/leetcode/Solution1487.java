37a4683c4ff68c4565f2c0c18fc6559925ab60c0
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-03-03 00:12:40 +0800

    1487. 保证文件名唯一

---
+++ src/leetcode/Solution1487.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1487. 保证文件名唯一
+ */
+public class Solution1487 {
+    public String[] getFolderNames(String[] names) {
+        Map<String, Integer> index = new HashMap<String, Integer>();
+        int n = names.length;
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            String name = names[i];
+            if (!index.containsKey(name)) {
+                res[i] = name;
+                index.put(name, 1);
+            } else {
+                int k = index.get(name);
+                while (index.containsKey(addSuffix(name, k))) {
+                    k++;
+                }
+                res[i] = addSuffix(name, k);
+                index.put(name, k + 1);
+                index.put(addSuffix(name, k), 1);
+            }
+        }
+        return res;
+    }
+
+    public String addSuffix(String name, int k) {
+        return name + "(" + k + ")";
+    }
+}
