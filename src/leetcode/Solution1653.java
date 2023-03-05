eaf58566266051c59a050737bc9260af6be07dc7
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-03-06 00:15:45 +0800

    1653. 使字符串平衡的最少删除次数

---
+++ src/leetcode/Solution1653.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * 1653. 使字符串平衡的最少删除次数
+ */
+public class Solution1653 {
+    public int minimumDeletions(String s) {
+        int leftb = 0, righta = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                righta++;
+            }
+        }
+        int res = righta;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a') {
+                righta--;
+            } else {
+                leftb++;
+            }
+            res = Math.min(res, leftb + righta);
+        }
+        return res;
+    }
+}
