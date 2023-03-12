2e7dcc80471393b519e0d9b7b964f16e349efde1
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-03-13 00:25:48 +0800

    2383. 赢得比赛需要的最少训练时长

---
+++ src/leetcode/Solution2383.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 2383. 赢得比赛需要的最少训练时长
+ */
+public class Solution2383 {
+    public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
+        int sum = 0;
+        for (int e : energy) {
+            sum += e;
+        }
+        int trainingHours = initialEnergy > sum ? 0 : sum + 1 - initialEnergy;
+        for (int e : experience) {
+            if (initialExperience <= e) {
+                trainingHours += 1 + (e - initialExperience);
+                initialExperience = 2 * e + 1;
+            } else {
+                initialExperience += e;
+            }
+        }
+        return trainingHours;
+    }
+}
