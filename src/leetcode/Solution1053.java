cbf3f1ad3005e79359280ae2f4afe288ea257c80
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-04-03 13:26:16 +0800

    1053. 交换一次的先前排列

---
+++ src/leetcode/Solution1053.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 1053. 交换一次的先前排列
+ */
+public class Solution1053 {
+    public int[] prevPermOpt1(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1]) {
+                int j = n - 1;
+                while (arr[j] >= arr[i] || arr[j] == arr[j - 1]) {
+                    j--;
+                }
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                break;
+            }
+        }
+        return arr;
+    }
+}
