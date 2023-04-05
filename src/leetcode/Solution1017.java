85cea9ecd7976ee40e96489c7a53b11f6f4989c4
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-04-06 00:09:50 +0800

    1017. 负二进制转换

---
+++ src/leetcode/Solution1017.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 1017. 负二进制转换
+ */
+public class Solution1017 {
+    public String baseNeg2(int n) {
+        if (n == 0 || n == 1) {
+            return String.valueOf(n);
+        }
+        StringBuilder res = new StringBuilder();
+        while (n != 0) {
+            int remainder = n & 1;
+            res.append(remainder);
+            n -= remainder;
+            n /= -2;
+        }
+        return res.reverse().toString();
+    }
+}
