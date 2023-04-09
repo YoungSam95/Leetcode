ce88ae6af7854b25e01bc488c01f81b4437f6a6e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-04-09 09:13:28 +0800

    2399. 检查相同字母间的距离

---
+++ src/leetcode/Solution2399.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * 2399. 检查相同字母间的距离
+ */
+public class Solution2399 {
+    public boolean checkDistances(String s, int[] distance) {
+        int[] firstIndex = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            if (firstIndex[idx] != 0 && i - firstIndex[idx] != distance[idx]) {
+                return false;
+            }
+            firstIndex[idx] = i + 1;
+        }
+        return true;
+    }
+}
