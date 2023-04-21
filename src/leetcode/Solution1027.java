97b07a1f4dbbae6de59c295f52f9cb01d4354827
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-04-22 01:24:07 +0800

    1027. 最长等差数列

---
+++ src/leetcode/Solution1027.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 1027. 最长等差数列
+ */
+public class Solution1027 {
+    public int longestArithSeqLength(int[] nums) {
+        int minv = Arrays.stream(nums).min().getAsInt();
+        int maxv = Arrays.stream(nums).max().getAsInt();
+        int diff = maxv - minv;
+        int ans = 1;
+        for (int d = -diff; d <= diff; ++d) {
+            int[] f = new int[maxv + 1];
+            Arrays.fill(f, -1);
+            for (int num : nums) {
+                int prev = num - d;
+                if (prev >= minv && prev <= maxv && f[prev] != -1) {
+                    f[num] = Math.max(f[num], f[prev] + 1);
+                    ans = Math.max(ans, f[num]);
+                }
+                f[num] = Math.max(f[num], 1);
+            }
+        }
+        return ans;
+    }
+}
