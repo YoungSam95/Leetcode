136786d0d47696bad91fa3268c7d6792d24c0f6d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-04-24 13:08:33 +0800

    1163. 按字典序排在最后的子串

---
+++ src/leetcode/Solution1163.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 1163. 按字典序排在最后的子串
+ */
+public class Solution1163 {
+    public String lastSubstring(String s) {
+        int i = 0, j = 1, n = s.length();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s.charAt(i + k) == s.charAt(j + k)) {
+                k++;
+            }
+            if (j + k < n && s.charAt(i + k) < s.charAt(j + k)) {
+                int t = i;
+                i = j;
+                j = Math.max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+        }
+        return s.substring(i);
+    }
+}
