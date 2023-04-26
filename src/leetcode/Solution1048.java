46e8005a6279abe4ed255507f2e498bee2d80af2
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-04-27 00:43:49 +0800

    1048. 最长字符串链

---
+++ src/leetcode/Solution1048.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * 1048. 最长字符串链
+ */
+public class Solution1048 {
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int res = 0;
+        for (String word : words) {
+            cnt.put(word, 1);
+            for (int i = 0; i < word.length(); i++) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                if (cnt.containsKey(prev)) {
+                    cnt.put(word, Math.max(cnt.get(word), cnt.get(prev) + 1));
+                }
+            }
+            res = Math.max(res, cnt.get(word));
+        }
+        return res;
+    }
+}
