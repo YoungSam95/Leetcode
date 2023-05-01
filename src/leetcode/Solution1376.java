1b36f685bbd8e7dc75ba86620ca7ff4636bf98a6
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-05-01 17:28:29 +0800

    1376. 通知所有员工所需的时间

---
+++ src/leetcode/Solution1376.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * 1376. 通知所有员工所需的时间
+ */
+public class Solution1376 {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        // 使用 HashMap 来构建图
+        Map<Integer, List<Integer>> g = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < n; i++) {
+            g.putIfAbsent(manager[i], new ArrayList<Integer>());
+            g.get(manager[i]).add(i);
+        }
+        // 从根节点开始进行 DFS 并返回总时间
+        return dfs(headID, informTime, g);
+    }
+
+    public int dfs(int cur, int[] informTime, Map<Integer, List<Integer>> g) {
+        int res = 0;
+        // 遍历当前节点的邻居节点
+        for (int neighbor : g.getOrDefault(cur, new ArrayList<Integer>())) {
+            res = Math.max(res, dfs(neighbor, informTime, g));
+        }
+        // 返回当前节点被通知需要的时间以及所有邻居节点被通知所需的最大时间
+        return informTime[cur] + res;
+    }
+}
