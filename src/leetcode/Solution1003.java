0b5b1633c807521d72c7cc685b54181f65c7218e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-05-03 11:54:52 +0800

    1003.检查替换后的词是否有效

---
+++ src/leetcode/Solution1003.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * 1003. 检查替换后的词是否有效
+ */
+public class Solution1003 {
+    public boolean isValid(String s) {
+        StringBuilder stk = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            stk.append(c);
+            if (stk.length() >= 3 && stk.substring(stk.length() - 3).equals("abc")) {
+                stk.delete(stk.length() - 3, stk.length());
+            }
+        }
+        return stk.length() == 0;
+    }
+}
