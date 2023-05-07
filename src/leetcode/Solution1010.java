d46e1c7acfdf71b418fb5de5a9c4431963586c6d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-05-07 11:44:58 +0800

    1010. 总持续时间可被 60 整除的歌曲

---
+++ src/leetcode/Solution1010.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 1010. 总持续时间可被 60 整除的歌曲
+ */
+public class Solution1010 {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] cnt = new int[60];
+        for (int t : time) {
+            cnt[t % 60]++;
+        }
+        long res = 0;
+        for (int i = 1; i < 30; i++) {
+            res += cnt[i] * cnt[60 - i];
+        }
+        res += (long) cnt[0] * (cnt[0] - 1) / 2 + (long) cnt[30] * (cnt[30] - 1) / 2;
+        return (int) res;
+    }
+}
