9861f2f005818decedf8bfff0ccbdeaf291ad22e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-05-22 14:06:21 +0800

    1080. 根到叶路径上的不足节点

---
+++ src/leetcode/Solution1080.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import common.TreeNode;
+
+/**
+ * 1080. 根到叶路径上的不足节点
+ */
+public class Solution1080 {
+    public TreeNode sufficientSubset(TreeNode root, int limit) {
+        boolean haveSufficient = checkSufficientLeaf(root, 0, limit);
+        return haveSufficient ? root : null;
+    }
+
+    public boolean checkSufficientLeaf(TreeNode node, int sum, int limit) {
+        if (node == null) {
+            return false;
+        }
+        if (node.left == null && node.right == null) {
+            return node.val + sum >= limit;
+        }
+        boolean haveSufficientLeft = checkSufficientLeaf(node.left, sum + node.val, limit);
+        boolean haveSufficientRight = checkSufficientLeaf(node.right, sum + node.val, limit);
+        if (!haveSufficientLeft) {
+            node.left = null;
+        }
+        if (!haveSufficientRight) {
+            node.right = null;
+        }
+        return haveSufficientLeft || haveSufficientRight;
+    }
+}
