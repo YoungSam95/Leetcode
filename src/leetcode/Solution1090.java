7205ed0e9e69d0feb42a7348430b8a4d1fa959d3
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-05-23 14:18:23 +0800

    1090. 受标签影响的最大值

---
+++ src/leetcode/Solution1090.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * 1090. 受标签影响的最大值
+ */
+public class Solution1090 {
+    public int largestValsFromLabels(int[] values, int[] labels, int numWanted, int useLimit) {
+        int n = values.length;
+        Integer[] id = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+        Arrays.sort(id, (a, b) -> values[b] - values[a]);
+
+        int ans = 0, choose = 0;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n && choose < numWanted; ++i) {
+            int label = labels[id[i]];
+            if (cnt.getOrDefault(label, 0) == useLimit) {
+                continue;
+            }
+            ++choose;
+            ans += values[id[i]];
+            cnt.put(label, cnt.getOrDefault(label, 0) + 1);
+        }
+        return ans;
+    }
+}
