7a223e89e63d94ed8421c6c086dac5e0d4239ebd
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-06-01 00:25:36 +0800

    2517. 礼盒的最大甜蜜度

---
+++ src/leetcode/Solution2517.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2517. 礼盒的最大甜蜜度
+ */
+public class Solution2517 {
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        int left = 0, right = price[price.length - 1] - price[0];
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (check(price, k, mid)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean check(int[] price, int k, int tastiness) {
+        int prev = Integer.MIN_VALUE / 2;
+        int cnt = 0;
+        for (int p : price) {
+            if (p - prev >= tastiness) {
+                cnt++;
+                prev = p;
+            }
+        }
+        return cnt >= k;
+    }
+}
