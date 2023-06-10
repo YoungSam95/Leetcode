9f808159ce1c98ea24b8055974fa4ae69f8f6655
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-06-10 11:04:31 +0800

    1170. 比较字符串最小字母出现频次

---
+++ src/leetcode/Solution1170.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * 1170. 比较字符串最小字母出现频次
+ */
+public class Solution1170 {
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        int[] count = new int[12];
+        for (String s : words) {
+            count[f(s)]++;
+        }
+        for (int i = 9; i >= 1; i--) {
+            count[i] += count[i + 1];
+        }
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String s = queries[i];
+            res[i] = count[f(s) + 1];
+        }
+        return res;
+    }
+
+    public int f(String s) {
+        int cnt = 0;
+        char ch = 'z';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c < ch) {
+                ch = c;
+                cnt = 1;
+            } else if (c == ch) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
