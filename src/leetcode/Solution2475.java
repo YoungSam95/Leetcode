1f849ac07faeaf9c4c1ceacae589ce3515f2ff62
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-06-13 17:58:32 +0800

    2475. 数组中不等三元组的数目

---
+++ src/leetcode/Solution2475.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 2475. 数组中不等三元组的数目
+ */
+public class Solution2475 {
+    public int unequalTriplets(int[] nums) {
+        int res = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
