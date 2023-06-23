930cd082f8c66f6450c1fd7ba3921cc9243468ea
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-06-23 18:09:46 +0800

    2496. 数组中字符串的最大值

---
+++ src/leetcode/Solution2496.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 2496. 数组中字符串的最大值
+ */
+public class Solution2496 {
+    public int maximumValue(String[] strs) {
+        int res = 0;
+        for (String s : strs) {
+            boolean isDigits = true;
+            int n = s.length();
+            for (int i = 0; i < n; ++i) {
+                isDigits &= Character.isDigit(s.charAt(i));
+            }
+            res = Math.max(res, isDigits ? Integer.parseInt(s) : n);
+        }
+        return res;
+    }
+}
