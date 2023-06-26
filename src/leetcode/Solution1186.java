07aec3c23e9fa74c5e4e79d04cd93c3e0e8f8d2b
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-06-27 00:40:15 +0800

    1186. 删除一次得到子数组最大和

---
+++ src/leetcode/Solution1186.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 1186. 删除一次得到子数组最大和
+ */
+public class Solution1186 {
+    public int maximumSum(int[] arr) {
+        int dp0 = arr[0], dp1 = 0, res = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            dp1 = Math.max(dp0, dp1 + arr[i]);
+            dp0 = Math.max(dp0, 0) + arr[i];
+            res = Math.max(res, Math.max(dp0, dp1));
+        }
+        return res;
+    }
+}
