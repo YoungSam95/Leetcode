be26e88179cc01b5892b754063baef95cab40172
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-07-02 17:52:14 +0800

    2.两数相加

--- src/leetcode/Solution2.java
+++ src/leetcode/Solution2.java
@@ -1,18 +1,12 @@
 package leetcode;
 
+import common.ListNode;
+
 /**
  * 2.两数相加
+ * 第二次
  */
 public class Solution2 {
-
-    public static class ListNode {
-
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
