c5c4c42a36fe69b3101bed74f1e4b4af71add570
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-07-04 13:09:43 +0800

    2679. 矩阵中的和

---
+++ src/leetcode/Solution2679.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2679. 矩阵中的和
+ */
+public class Solution2679 {
+    public int matrixSum(int[][] nums) {
+        int res = 0;
+        int m = nums.length;
+        int n = nums[0].length;
+        for (int i = 0; i < m; i++) {
+            Arrays.sort(nums[i]);
+        }
+        for (int j = 0; j < n; j++) {
+            int maxVal = 0;
+            for (int i = 0; i < m; i++) {
+                maxVal = Math.max(maxVal, nums[i][j]);
+            }
+            res += maxVal;
+        }
+        return res;
+    }
+}
