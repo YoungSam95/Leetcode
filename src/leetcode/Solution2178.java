bc0fac896b793e1365a4931682231d0a285798d5
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-07-06 00:16:28 +0800

    2178. 拆分成最多数目的正偶数之和

---
+++ src/leetcode/Solution2178.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2178. 拆分成最多数目的正偶数之和
+ */
+public class Solution2178 {
+    public List<Long> maximumEvenSplit(long finalSum) {
+        List<Long> res = new ArrayList<>();
+        if (finalSum % 2 > 0) {
+            return res;
+        }
+        for (long i = 2; i <= finalSum; i += 2) {
+            res.add(i);
+            finalSum -= i;
+        }
+        res.set(res.size() - 1, res.get(res.size() - 1) + finalSum);
+        return res;
+    }
+}
