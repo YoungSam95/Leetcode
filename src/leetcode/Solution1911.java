84c2376540a3334ae6706203938d1743e7e5fb09
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-07-11 13:40:31 +0800

    1911. 最大子序列交替和

---
+++ src/leetcode/Solution1911.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * 1911. 最大子序列交替和
+ */
+public class Solution1911 {
+    public long maxAlternatingSum(int[] nums) {
+        long even = nums[0], odd = 0;
+        for (int i = 1; i < nums.length; i++) {
+            even = Math.max(even, odd + nums[i]);
+            odd = Math.max(odd, even - nums[i]);
+        }
+        return even;
+    }
+}
