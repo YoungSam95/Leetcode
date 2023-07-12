273072d46b86c22820080aa9d9f9f47e28230d1c
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-07-13 01:00:41 +0800

    931. 下降路径最小和

---
+++ src/leetcode/Solution931.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 931. 下降路径最小和
+ */
+public class Solution931 {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        System.arraycopy(matrix[0], 0, dp[0], 0, n);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int mn = dp[i - 1][j];
+                if (j > 0) {
+                    mn = Math.min(mn, dp[i - 1][j - 1]);
+                }
+                if (j < n - 1) {
+                    mn = Math.min(mn, dp[i - 1][j + 1]);
+                }
+                dp[i][j] = mn + matrix[i][j];
+            }
+        }
+        return Arrays.stream(dp[n - 1]).min().getAsInt();
+    }
+}
