8781ac8d80d9260c5e1c342cfe0b020a4c8f13f7
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-07-22 00:20:09 +0800

    860. 柠檬水找零

---
+++ src/leetcode/Solution860.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 860. 柠檬水找零
+ */
+public class Solution860 {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
