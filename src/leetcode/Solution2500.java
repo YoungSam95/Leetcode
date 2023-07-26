65419cd819979e5ed7794c2b0b3b63fe0c5be03c
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-07-27 00:07:14 +0800

    2500. 删除每行中的最大值

---
+++ src/leetcode/Solution2500.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2500. 删除每行中的最大值
+ */
+public class Solution2500 {
+    public int deleteGreatestValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            Arrays.sort(grid[i]);
+        }
+        int res = 0;
+        for (int j = 0; j < n; j++) {
+            int mx = 0;
+            for (int i = 0; i < m; i++) {
+                mx = Math.max(mx, grid[i][j]);
+            }
+            res += mx;
+        }
+        return res;
+    }
+}
