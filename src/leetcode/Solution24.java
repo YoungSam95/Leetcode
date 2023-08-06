5ac3ebd23eb3e50c28e8a08b408848aec8131c67
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-08-06 14:38:49 +0800

    24. 两两交换链表中的节点

---
+++ src/leetcode/Solution24.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import common.ListNode;
+
+/**
+ * 24. 两两交换链表中的节点
+ */
+public class Solution24 {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode first = head;
+        ListNode second = head.next;
+        ListNode others = head.next.next;
+        // 先把前两个元素翻转
+        second.next = first;
+        // 利用递归定义，将剩下的链表节点两两翻转，接到后面
+        first.next = swapPairs(others);
+        // 现在整个链表都成功翻转了，返回新的头结点
+        return second;
+    }
+}
