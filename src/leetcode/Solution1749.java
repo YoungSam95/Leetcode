8adcd6cb45ba3552b5573d8b06a9df67ca3521c3
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-08-08 14:18:08 +0800

    1749. 任意子数组和的绝对值的最大值

---
+++ src/leetcode/Solution1749.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 1749. 任意子数组和的绝对值的最大值
+ */
+public class Solution1749 {
+    public int maxAbsoluteSum(int[] nums) {
+        int positiveMax = 0, negativeMin = 0;
+        int positiveSum = 0, negativeSum = 0;
+        for (int num : nums) {
+            positiveSum += num;
+            positiveMax = Math.max(positiveMax, positiveSum);
+            positiveSum = Math.max(0, positiveSum);
+            negativeSum += num;
+            negativeMin = Math.min(negativeMin, negativeSum);
+            negativeSum = Math.min(0, negativeSum);
+        }
+        return Math.max(positiveMax, -negativeMin);
+    }
+}
