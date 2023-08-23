323ef95c6161ae2c29f91dbbcf5e69714686b054
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-08-24 00:30:15 +0800

    1267. 统计参与通信的服务器

---
+++ src/leetcode/Solution1267.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1267. 统计参与通信的服务器
+ */
+public class Solution1267 {
+    public int countServers(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Map<Integer, Integer> rows = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> cols = new HashMap<Integer, Integer>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    rows.put(i, rows.getOrDefault(i, 0) + 1);
+                    cols.put(j, cols.getOrDefault(j, 0) + 1);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && (rows.get(i) > 1 || cols.get(j) > 1)) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
