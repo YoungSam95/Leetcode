a06fcb3cdd1733c7a038fe7b728a4e8587aecec5
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-09-01 11:50:10 +0800

    2240. 买钢笔和铅笔的方案数

---
+++ src/leetcode/Solution2240.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * 2240. 买钢笔和铅笔的方案数
+ */
+public class Solution2240 {
+    public long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        if (cost1 < cost2) {
+            return waysToBuyPensPencils(total, cost2, cost1);
+        }
+        long res = 0, cnt = 0;
+        while (cnt * cost1 <= total) {
+            res += (total - cnt * cost1) / cost2 + 1;
+            cnt++;
+        }
+        return res;
+    }
+}
