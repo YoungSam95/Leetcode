87e1455cc75eb518e1c2e21e29109b18560a7593
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-09-03 12:35:55 +0800

    1921. 消灭怪物的最大数量

---
+++ src/leetcode/Solution1921.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 1921. 消灭怪物的最大数量
+ */
+public class Solution1921 {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.length;
+        int[] arrivalTimes = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrivalTimes[i] = (dist[i] - 1) / speed[i] + 1;
+        }
+        Arrays.sort(arrivalTimes);
+        for (int i = 0; i < n; i++) {
+            if (arrivalTimes[i] <= i) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
