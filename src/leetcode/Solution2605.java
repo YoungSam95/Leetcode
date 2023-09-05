98388ae9f76921d14acb51861ef1de78644d3336
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-09-05 13:01:17 +0800

    2605. 从两个数字数组里生成最小数字

---
+++ src/leetcode/Solution2605.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 2605. 从两个数字数组里生成最小数字
+ */
+public class Solution2605 {
+    public int minNumber(int[] nums1, int[] nums2) {
+        int s = same(nums1, nums2);
+        if (s != -1) {
+            return s;
+        }
+
+        int x = Arrays.stream(nums1).min().getAsInt();
+        int y = Arrays.stream(nums2).min().getAsInt();
+        return Math.min(x * 10 + y, y * 10 + x);
+    }
+
+    public int same(int[] nums1, int[] nums2) {
+        Set<Integer> s = new HashSet<Integer>();
+        for (int num : nums1) {
+            s.add(num);
+        }
+        int x = 10;
+        for (int num : nums2) {
+            if (s.contains(num)) {
+                x = Math.min(x, num);
+            }
+        }
+        return x == 10 ? -1 : x;
+    }
+}
