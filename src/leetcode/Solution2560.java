a413fc50656cdf62257d9772d982f4b1ffdbf675
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-09-19 12:42:36 +0800

    2560. 打家劫舍 IV

---
+++ src/leetcode/Solution2560.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2560. 打家劫舍 IV
+ */
+public class Solution2560 {
+    public int minCapability(int[] nums, int k) {
+        int lower = Arrays.stream(nums).min().getAsInt();
+        int upper = Arrays.stream(nums).max().getAsInt();
+        while (lower <= upper) {
+            int middle = (lower + upper) / 2;
+            int count = 0;
+            boolean visited = false;
+            for (int x : nums) {
+                if (x <= middle && !visited) {
+                    count++;
+                    visited = true;
+                } else {
+                    visited = false;
+                }
+            }
+            if (count >= k) {
+                upper = middle - 1;
+            } else {
+                lower = middle + 1;
+            }
+        }
+        return lower;
+    }
+}
