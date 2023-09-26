6c456d05ad0a717633f87c059c7c352c898e0a2c
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-09-27 00:19:45 +0800

    1333. 餐厅过滤器

---
+++ src/leetcode/Solution1333.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1333. 餐厅过滤器
+ */
+public class Solution1333 {
+    public List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance) {
+        int n = restaurants.length;
+        List<int[]> filtered = new ArrayList<int[]>();
+        for (int i = 0; i < n; i++) {
+            if (restaurants[i][3] <= maxPrice && restaurants[i][4] <= maxDistance && !(veganFriendly == 1 && restaurants[i][2] == 0)) {
+                filtered.add(restaurants[i]);
+            }
+        }
+        Collections.sort(filtered, (a, b) -> {
+            if (a[1] != b[1]) {
+                return b[1] - a[1];
+            } else {
+                return b[0] - a[0];
+            }
+        });
+        List<Integer> res = new ArrayList<Integer>();
+        for (int[] v : filtered) {
+            res.add(v[0]);
+        }
+        return res;
+    }
+}
