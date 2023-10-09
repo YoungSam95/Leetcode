0c7b460ea0a1446e9c73551164e5238e6f2f46f9
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-10-09 17:22:50 +0800

    2578. 最小和分割

---
+++ src/leetcode/Solution2578.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2578. 最小和分割
+ */
+public class Solution2578 {
+    public int splitNum(int num) {
+        char[] stnum = Integer.toString(num).toCharArray();
+        Arrays.sort(stnum);
+        int num1 = 0, num2 = 0;
+        for (int i = 0; i < stnum.length; ++i) {
+            if (i % 2 == 0) {
+                num1 = num1 * 10 + (stnum[i] - '0');
+            } else {
+                num2 = num2 * 10 + (stnum[i] - '0');
+            }
+        }
+        return num1 + num2;
+    }
+}
