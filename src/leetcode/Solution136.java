1282bbdcc821ad3d92cc7caaec02e5629a7cc373
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-10-14 00:14:52 +0800

    136. 只出现一次的数字

---
+++ src/leetcode/Solution136.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * 136. 只出现一次的数字
+ */
+public class Solution136 {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
