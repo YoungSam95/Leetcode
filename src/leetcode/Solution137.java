e6171f65a581afb8962a844a1829c84f096a3c44
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-10-15 12:45:59 +0800

    137. 只出现一次的数字 II

---
+++ src/leetcode/Solution137.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 137. 只出现一次的数字 II
+ */
+public class Solution137 {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int num = entry.getKey(), occ = entry.getValue();
+            if (occ == 1) {
+                ans = num;
+                break;
+            }
+        }
+        return ans;
+    }
+}
