5f442cba91001edb20e769d8ed5f7be23128b76b
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-10-17 00:21:04 +0800

    2652. 倍数求和

---
+++ src/leetcode/Solution2652.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 2652. 倍数求和
+ */
+public class Solution2652 {
+    public int sumOfMultiples(int n) {
+        int res = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+                res += i;
+            }
+        }
+        return res;
+    }
+}
