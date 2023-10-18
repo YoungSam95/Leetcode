20775efb35c78b0eb7f129283131e928b5a5f2e2
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-10-19 00:08:41 +0800

    1726. 同积元组

---
+++ src/leetcode/Solution1726.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1726. 同积元组
+ */
+public class Solution1726 {
+    public int tupleSameProduct(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int key = nums[i] * nums[j];
+                cnt.put(key, cnt.getOrDefault(key, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (Integer v : cnt.values()) {
+            ans += v * (v - 1) * 4;
+        }
+        return ans;
+    }
+}
