e62878d6818fb42d1a8616aa338acca7887a87bd
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-10-20 00:03:40 +0800

    2525. 根据规则将箱子分类

---
+++ src/leetcode/Solution2525.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 2525. 根据规则将箱子分类
+ */
+public class Solution2525 {
+    public String categorizeBox(int length, int width, int height, int mass) {
+        long maxd = Math.max(length, Math.max(width, height)), vol = 1L * length * width * height;
+        boolean isBulky = maxd >= 10000 || vol >= 1000000000, isHeavy = mass >= 100;
+        if (isBulky && isHeavy) {
+            return "Both";
+        } else if (isBulky) {
+            return "Bulky";
+        } else if (isHeavy) {
+            return "Heavy";
+        } else {
+            return "Neither";
+        }
+    }
+}
