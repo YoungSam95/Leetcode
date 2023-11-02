7dd8ce565047a0afafecf0406acef460a05c8a23
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-11-02 17:17:47 +0800

    2103. 环和杆

---
+++ src/leetcode/Solution2103.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+/**
+ * 2103. 环和杆
+ */
+public class Solution2103 {
+    static final int POLE_NUM = 10;
+    static final int COLOR_NUM = 3;
+
+    public int countPoints(String rings) {
+        int[][] state = new int[POLE_NUM][COLOR_NUM];
+        int n = rings.length();
+        for (int i = 0; i < n; i += 2) {
+            char color = rings.charAt(i);
+            int poleIndex = rings.charAt(i + 1) - '0';
+            state[poleIndex][getColorId(color)] = 1;
+        }
+        int res = 0;
+        for (int i = 0; i < POLE_NUM; i++) {
+            boolean flag = true;
+            for (int j = 0; j < COLOR_NUM; j++) {
+                if (state[i][j] == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public int getColorId(char color) {
+        if (color == 'R') {
+            return 0;
+        } else if (color == 'G') {
+            return 1;
+        }
+        return 2;
+    }
+}
