31c231e89bb56a063123c540564cb73957fceb07
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-11-21 17:10:43 +0800

    2216. 美化数组的最少删除数

---
+++ src/leetcode/Solution2216.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 2216. 美化数组的最少删除数
+ */
+public class Solution2216 {
+    public int minDeletion(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        boolean check = true;
+        for (int i = 0; i + 1 < n; ++i) {
+            if (nums[i] == nums[i + 1] && check) {
+                ++ans;
+            } else {
+                check = !check;
+            }
+        }
+        if ((n - ans) % 2 != 0) {
+            ++ans;
+        }
+        return ans;
+    }
+}
