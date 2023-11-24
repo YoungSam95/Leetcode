2ae1ab1ed8976de0ce2bdfe94077429d80f8b0cf
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-11-24 18:10:52 +0800

    2824. 统计和小于目标的下标对数目

---
+++ src/leetcode/Solution2824.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * 2824. 统计和小于目标的下标对数目
+ */
+public class Solution2824 {
+    public int countPairs(List<Integer> nums, int target) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
