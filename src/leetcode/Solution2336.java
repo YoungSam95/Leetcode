a70ce14f9b883fae25b86a6ec7dd8d30048f7a6b
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-11-29 17:21:32 +0800

    2336. 无限集中的最小数字

---
+++ src/leetcode/Solution2336.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.TreeSet;
+
+/**
+ * 2336. 无限集中的最小数字
+ */
+public class Solution2336 {
+    class SmallestInfiniteSet {
+        private int thres;
+        private TreeSet<Integer> set;
+
+        public SmallestInfiniteSet() {
+            thres = 1;
+            set = new TreeSet<Integer>();
+        }
+
+        public int popSmallest() {
+            if (set.isEmpty()) {
+                int ans = thres;
+                ++thres;
+                return ans;
+            }
+            int ans = set.pollFirst();
+            return ans;
+        }
+
+        public void addBack(int num) {
+            if (num < thres) {
+                set.add(num);
+            }
+        }
+    }
+}
