f871b600eede295293ec5b11b20b1dfd4ae02fe3
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-12-01 17:52:31 +0800

    2661. 找出叠涂元素

---
+++ src/leetcode/Solution2661.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 2661. 找出叠涂元素
+ */
+public class Solution2661 {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        Map<Integer, int[]> map = new HashMap<Integer, int[]>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                map.put(mat[i][j], new int[]{i, j});
+            }
+        }
+        int[] rowCnt = new int[n];
+        int[] colCnt = new int[m];
+        for (int i = 0; i < arr.length; ++i) {
+            int[] v = map.get(arr[i]);
+            ++rowCnt[v[0]];
+            if (rowCnt[v[0]] == m) {
+                return i;
+            }
+            ++colCnt[v[1]];
+            if (colCnt[v[1]] == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
