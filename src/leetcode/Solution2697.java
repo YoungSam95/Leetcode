a76b96fb44317c33d177098d4aead57082f5ef54
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-12-13 12:47:04 +0800

    2697. 字典序最小回文串

---
+++ src/leetcode/Solution2697.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 2697. 字典序最小回文串
+ */
+public class Solution2697 {
+    public String makeSmallestPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                arr[left] = arr[right] = (char) Math.min(arr[left], arr[right]);
+            }
+            ++left;
+            --right;
+        }
+        return new String(arr);
+    }
+}
