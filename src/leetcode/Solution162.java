dff579b13a8f6a08de0a766ba1f32b2924c4bcf1
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-12-18 11:21:53 +0800

    162. 寻找峰值

---
+++ src/leetcode/Solution162.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 162. 寻找峰值
+ */
+public class Solution162 {
+    public int findPeakElement(int[] nums) {
+        int idx = 0;
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] > nums[idx]) {
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
