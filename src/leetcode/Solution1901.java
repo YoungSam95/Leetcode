06db94e724a1686c70a6d09511a36941934d3599
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-12-19 17:47:51 +0800

    1901. 寻找峰值 II

---
+++ src/leetcode/Solution1901.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 1901. 寻找峰值 II
+ */
+public class Solution1901 {
+    public int[] findPeakGrid(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int low = 0, high = m - 1;
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = -1, maxElement = -1;
+            for (int k = 0; k < n; k++) {
+                if (mat[i][k] > maxElement) {
+                    j = k;
+                    maxElement = mat[i][k];
+                }
+            }
+            if (i - 1 >= 0 && mat[i][j] < mat[i - 1][j]) {
+                high = i - 1;
+                continue;
+            }
+            if (i + 1 < m && mat[i][j] < mat[i + 1][j]) {
+                low = i + 1;
+                continue;
+            }
+            return new int[]{i, j};
+        }
+        return new int[0]; // impossible
+    }
+}
