ff6d8fec97ef93c4728263d0f39a8336afa5290d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-12-24 17:27:40 +0800

    1954. 收集足够苹果的最小花园周长

---
+++ src/leetcode/Solution1954.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 1954. 收集足够苹果的最小花园周长
+ */
+public class Solution1954 {
+    public long minimumPerimeter(long neededApples) {
+        long left = 1, right = 100000, ans = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            if (2 * mid * (mid + 1) * (mid * 2 + 1) >= neededApples) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans * 8;
+    }
+}
