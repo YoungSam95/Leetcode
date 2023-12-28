931f3ae441432216a83acbe3f1189d5aa08bf011
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2023-12-28 17:43:03 +0800

    2735. 收集巧克力

---
+++ src/leetcode/Solution2735.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * 2735. 收集巧克力
+ */
+public class Solution2735 {
+    public long minCost(int[] nums, int x) {
+        int n = nums.length;
+        int[] f = new int[n];
+        System.arraycopy(nums, 0, f, 0, n);
+        long ans = getSum(f);
+        for (int k = 1; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                f[i] = Math.min(f[i], nums[(i + k) % n]);
+            }
+            ans = Math.min(ans, (long) k * x + getSum(f));
+        }
+        return ans;
+    }
+
+    public long getSum(int[] f) {
+        long sum = 0;
+        for (int num : f) {
+            sum += num;
+        }
+        return sum;
+    }
+}
