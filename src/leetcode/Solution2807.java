e8ad81a5d15d18b59a5b08c214ca1e4b21399c4d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-01-06 17:22:31 +0800

    2807. 在链表中插入最大公约数

---
+++ src/leetcode/Solution2807.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import common.ListNode;
+
+/**
+ * 2807. 在链表中插入最大公约数
+ */
+public class Solution2807 {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode node = head;
+        while (node.next != null) {
+            node.next = new ListNode(gcd(node.val, node.next.val), node.next);
+            node = node.next.next;
+        }
+        return head;
+    }
+
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
