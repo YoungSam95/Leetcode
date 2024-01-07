186e239f5e394553b1c475e69fa74e056f70b448
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-01-07 12:29:57 +0800

    383. 赎金信

---
+++ src/leetcode/Solution383.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 383. 赎金信
+ */
+public class Solution383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (char c : magazine.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            cnt[c - 'a']--;
+            if(cnt[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
