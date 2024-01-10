d53918a5607d676108ea004787a245fdd7c282e7
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-01-10 12:35:09 +0800

    2696. 删除子串后的字符串最小长度

---
+++ src/leetcode/Solution2696.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2696. 删除子串后的字符串最小长度
+ */
+public class Solution2696 {
+    public int minLength(String s) {
+        List<Character> stack = new ArrayList<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            stack.add(c);
+            int m = stack.size();
+            if (m >= 2 &&
+                    (stack.get(m - 2) == 'A' && stack.get(m - 1) == 'B' ||
+                            stack.get(m - 2) == 'C' && stack.get(m - 1) == 'D')) {
+                stack.remove(m - 1);
+                stack.remove(m - 2);
+            }
+        }
+        return stack.size();
+    }
+}
