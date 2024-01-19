23e89eb63e4a2d90f51c91cc0fdedd53410cbfc9
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-01-20 00:10:20 +0800

    2788. 按分隔符拆分字符串

---
+++ src/leetcode/Solution2788.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2788. 按分隔符拆分字符串
+ */
+public class Solution2788 {
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> res = new ArrayList<String>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            int length = word.length();
+            for (int i = 0; i < length; i++) {
+                char c = word.charAt(i);
+                if (c == separator) {
+                    if (sb.length() > 0) {
+                        res.add(sb.toString());
+                        sb.setLength(0);
+                    }
+                } else {
+                    sb.append(c);
+                }
+            }
+            if (sb.length() > 0) {
+                res.add(sb.toString());
+            }
+        }
+        return res;
+    }
+}
