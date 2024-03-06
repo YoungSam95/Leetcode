c48fe05eb9ffc6ce990c6ed539c6322d6d7750d4
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-03-07 00:36:36 +0800

    2575. 找出字符串的可整除数组

---
+++ src/leetcode/Solution2575.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * 2575. 找出字符串的可整除数组
+ */
+public class Solution2575 {
+    public int[] divisibilityArray(String word, int m) {
+        int[] res = new int[word.length()];
+        long cur = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            cur = (cur * 10 + (c - '0')) % m;
+            res[i] = (cur == 0) ? 1 : 0;
+        }
+        return res;
+    }
+}
