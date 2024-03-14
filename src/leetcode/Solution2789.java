269680f7ba7bcfd5b5f1cfe40e524f53fbe053e8
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-03-14 11:53:18 +0800

    2789. 合并后数组中的最大元素

---
+++ src/leetcode/Solution2789.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * 2789. 合并后数组中的最大元素
+ */
+public class Solution2789 {
+    public long maxArrayValue(int[] nums) {
+        long sum = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            sum = nums[i] <= sum ? nums[i] + sum : nums[i];
+        }
+        return sum;
+    }
+}
