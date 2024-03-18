ea977ddc7d8975e1773d8fb6f25a8a512b28b376
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-03-18 10:04:16 +0800

    303.区域和检索-数组不可变

---
+++ src/leetcode/Solution303.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * 303.区域和检索-数组不可变
+ */
+public class Solution303 {
+    class NumArray {
+        int[] sums;
+
+        public NumArray(int[] nums) {
+            int n = nums.length;
+            sums = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                sums[i + 1] = sums[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int i, int j) {
+            return sums[j + 1] - sums[i];
+        }
+    }
+}
