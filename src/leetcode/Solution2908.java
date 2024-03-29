0d82d9642cb4cd5a552d72b0f488e8233903d42f
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-03-29 11:58:49 +0800

    2908. 元素和最小的山形三元组 I

---
+++ src/leetcode/Solution2908.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 2908. 元素和最小的山形三元组 I
+ */
+public class Solution2908 {
+    public int minimumSum(int[] nums) {
+        int n = nums.length, res = 1000, mn = 1000;
+        int[] left = new int[n];
+        for (int i = 1; i < n; i++) {
+            left[i] = mn = Math.min(nums[i - 1], mn);
+        }
+
+        int right = nums[n - 1];
+        for (int i = n - 2; i > 0; i--) {
+            if (left[i] < nums[i] && nums[i] > right) {
+                res = Math.min(res, left[i] + nums[i] + right);
+            }
+            right = Math.min(right, nums[i]);
+        }
+
+        return res < 1000 ? res : -1;
+    }
+}
