8bdd14cc93ee8a46bd73045594304b59edb1b121
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-03-30 11:37:07 +0800

    2952. 需要添加的硬币的最小数量

---
+++ src/leetcode/Solution2952.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2952. 需要添加的硬币的最小数量
+ */
+public class Solution2952 {
+    public int minimumAddedCoins(int[] coins, int target) {
+        Arrays.sort(coins);
+        int ans = 0;
+        int x = 1;
+        int length = coins.length, index = 0;
+        while (x <= target) {
+            if (index < length && coins[index] <= x) {
+                x += coins[index];
+                index++;
+            } else {
+                x *= 2;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
