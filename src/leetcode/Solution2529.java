f094fd37f56937d9ea2434b03707d945506ad054
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-04-09 12:47:36 +0800

    2529. 正整数和负整数的最大计数

---
+++ src/leetcode/Solution2529.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * 2529. 正整数和负整数的最大计数
+ */
+public class Solution2529 {
+    public int maximumCount(int[] nums) {
+        int pos = 0, neg = 0;
+        for (int num : nums) {
+            if (num > 0) {
+                pos++;
+            } else if (num < 0) {
+                neg++;
+            }
+        }
+        return Math.max(pos, neg);
+    }
+}
