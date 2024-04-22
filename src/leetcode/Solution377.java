0b96a3b41a56fb53b44887fe6bd05a39837dce3a
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-04-22 11:58:37 +0800

    377. 组合总和 Ⅳ

---
+++ src/leetcode/Solution377.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 377. 组合总和 Ⅳ
+ */
+public class Solution377 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
