cc2dbe256b6234aea5db106b5c5125f4d9f36d6f
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-05-22 00:04:15 +0800

    2225. 找出输掉零场或一场比赛的玩家

---
+++ src/leetcode/Solution2225.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * 2225. 找出输掉零场或一场比赛的玩家
+ */
+public class Solution2225 {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int[] match : matches) {
+            int winner = match[0], loser = match[1];
+            freq.putIfAbsent(winner, 0);
+            freq.put(loser, freq.getOrDefault(loser, 0) + 1);
+        }
+
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        for (int i = 0; i < 2; ++i) {
+            ans.add(new ArrayList<Integer>());
+        }
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int key = entry.getKey(), value = entry.getValue();
+            if (value < 2) {
+                ans.get(value).add(key);
+            }
+        }
+
+        Collections.sort(ans.get(0));
+        Collections.sort(ans.get(1));
+        return ans;
+    }
+}
