57202bb6e68df9dfdd6e0a93a002a1071ad6ad73
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-05-27 00:03:36 +0800

    2028. 找出缺失的观测数据

---
+++ src/leetcode/Solution2028.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 2028. 找出缺失的观测数据
+ */
+public class Solution2028 {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int m = rolls.length;
+        int sum = mean * (n + m);
+        int missingSum = sum;
+        for (int roll : rolls) {
+            missingSum -= roll;
+        }
+        if (missingSum < n || missingSum > 6 * n) {
+            return new int[0];
+        }
+        int quotient = missingSum / n, remainder = missingSum % n;
+        int[] missing = new int[n];
+        for (int i = 0; i < n; i++) {
+            missing[i] = quotient + (i < remainder ? 1 : 0);
+        }
+        return missing;
+    }
+}
