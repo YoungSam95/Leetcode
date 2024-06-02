37ab4629234251a44731b30c06f6906ac015953f
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-06-03 00:04:40 +0800

    1103. 分糖果 II

---
+++ src/leetcode/Solution1103.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * 1103. 分糖果 II
+ */
+public class Solution1103 {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ans = new int[num_people];
+        int i = 0;
+        while (candies != 0) {
+            ans[i % num_people] += Math.min(candies, i + 1);
+            candies -= Math.min(candies, i + 1);
+            i += 1;
+        }
+        return ans;
+    }
+}
