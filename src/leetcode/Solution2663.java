1b10e225b5dda3fedd3e1908a8dbd91cc26e5707
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-06-22 00:07:22 +0800

    2663. 字典序最小的美丽字符串

---
+++ src/leetcode/Solution2663.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 2663. 字典序最小的美丽字符串
+ */
+public class Solution2663 {
+    public String smallestBeautifulString(String s, int k) {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            Set<Character> blockedCharacters = new HashSet<Character>();
+            for (int j = 1; j < 3; j++) {
+                if (i - j >= 0) {
+                    blockedCharacters.add(s.charAt(i - j));
+                }
+            }
+            for (int j = 1; j < 4; j++) {
+                if (s.charAt(i) - 'a' + j + 1 <= k && !blockedCharacters.contains((char) (s.charAt(i) + j))) {
+                    return generate(s, i, j);
+                }
+            }
+        }
+        return "";
+    }
+
+    public String generate(String s, int idx, int offset) {
+        char[] res = s.toCharArray();
+        res[idx] += offset;
+        for (int i = idx + 1; i < s.length(); i++) {
+            Set<Character> blockedCharacters = new HashSet<Character>();
+            for (int j = 1; j < 3; j++) {
+                if (i - j >= 0) {
+                    blockedCharacters.add(res[i - j]);
+                }
+            }
+            for (int j = 0; j < 3; j++) {
+                if (!blockedCharacters.contains((char) ('a' + j))) {
+                    res[i] = (char) ('a' + j);
+                    break;
+                }
+            }
+        }
+        return new String(res);
+    }
+
+}
