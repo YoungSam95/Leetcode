73cf00274d4d33ae84713af1fdd9e1265e4605fb
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-06-24 00:03:23 +0800

    503. 下一个更大元素 II

---
+++ src/leetcode/Solution503.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 503. 下一个更大元素 II
+ */
+public class Solution503 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        Arrays.fill(ret, -1);
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = 0; i < n * 2 - 1; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+                ret[stack.pop()] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return ret;
+    }
+}
