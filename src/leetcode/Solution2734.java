121ccf810cb4b0ecd215f5a81ba9f116d5fe9293
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-06-27 00:03:38 +0800

    2734. 执行子串操作后的字典序最小字符串

---
+++ src/leetcode/Solution2734.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+/**
+ * 2734. 执行子串操作后的字典序最小字符串
+ */
+public class Solution2734 {
+    public String smallestString(String s) {
+        int indexOfFirstNonA = findFirstNonA(s);
+        if (indexOfFirstNonA == s.length()) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.setCharAt(s.length() - 1, 'z');
+            return sb.toString();
+        }
+        int indexOfFirstA_AfterFirstNonA = findFirstA_AfterFirstNonA(s, indexOfFirstNonA);
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (i >= indexOfFirstNonA && i < indexOfFirstA_AfterFirstNonA) {
+                res.append((char) (c - 1));
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+
+    public int findFirstNonA(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != 'a') {
+                return i;
+            }
+        }
+        return s.length();
+    }
+
+    public int findFirstA_AfterFirstNonA(String s, int firstNonA) {
+        for (int i = firstNonA; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                return i;
+            }
+        }
+        return s.length();
+    }
+}
