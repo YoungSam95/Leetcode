cfecf75f4e58f8e5c6955cda7f932e048a8ae526
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-07-08 00:05:17 +0800

    724. 寻找数组的中心下标

---
+++ src/leetcode/Solution724.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 724. 寻找数组的中心下标
+ */
+public class Solution724 {
+    public int pivotIndex(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+}
