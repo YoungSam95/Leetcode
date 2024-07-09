2b3b8acf1454029d1ec1272f2b75b3b79ae092ed
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-07-10 00:03:46 +0800

    2970. 统计移除递增子数组的数目 I

---
+++ src/leetcode/Solution2970.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * 2970. 统计移除递增子数组的数目 I
+ */
+public class Solution2970 {
+    public int incremovableSubarrayCount(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        int l = 1;
+        while (l < n && nums[l - 1] < nums[l]) {
+            l++;
+        }
+        res += l + (l < n ? 1 : 0);
+        for (int r = n - 2; r >= 0; r--) {
+            while (l > 0 && nums[l - 1] >= nums[r + 1]) {
+                l--;
+            }
+            res += l + (l <= r ? 1 : 0);
+            if (nums[r] >= nums[r + 1]) {
+                break;
+            }
+        }
+        return res;
+    }
+}
