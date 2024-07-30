418a8bccbf7c075e5348a36faa77c3d6f242077d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-07-31 00:14:56 +0800

    3111. 覆盖所有点的最少矩形数目

--- src/leetcode/Solution3111.java
+++ src/leetcode/Solution3111.java
@@ -1,2 +1,21 @@
-package leetcode;public class Solution3111 {
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 3111. 覆盖所有点的最少矩形数目
+ */
+public class Solution3111 {
+    public int minRectanglesToCoverPoints(int[][] points, int w) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int res = 0;
+        int bound = -1;
+        for (int[] p : points) {
+            if (p[0] > bound) {
+                bound = p[0] + w;
+                res++;
+            }
+        }
+        return res;
+    }
 }
