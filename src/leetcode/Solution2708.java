e9b89c656b025ffdc57396fd45dd21c8fd737ca6
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-09-03 00:03:19 +0800

    2708. 一个小组的最大实力值

---
+++ src/leetcode/Solution2708.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * 2708. 一个小组的最大实力值
+ */
+public class Solution2708 {
+    public long maxStrength(int[] nums) {
+        int negativeCount = 0, zeroCount = 0, positiveCount = 0;
+        long prod = 1;
+        int maxNegative = -9;
+        for (int num : nums) {
+            if (num < 0) {
+                negativeCount++;
+                prod *= num;
+                maxNegative = Math.max(maxNegative, num);
+            } else if (num == 0) {
+                zeroCount++;
+            } else {
+                prod *= num;
+                positiveCount++;
+            }
+        }
+        if (negativeCount == 1 && zeroCount == 0 && positiveCount == 0) {
+            return nums[0];
+        }
+        if (negativeCount <= 1 && positiveCount == 0) {
+            return 0;
+        }
+        if (prod < 0) {
+            return prod / maxNegative;
+        } else {
+            return prod;
+        }
+    }
+}
