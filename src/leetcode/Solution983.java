c39d84680384a78ceaacc298ef14e5f1d757773e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-10-01 00:04:11 +0800

    983. 最低票价

---
+++ src/leetcode/Solution983.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * 983. 最低票价
+ */
+public class Solution983 {
+    int[] days, costs;
+    Integer[] memo;
+    int[] durations = new int[]{1, 7, 30};
+
+    public int mincostTickets(int[] days, int[] costs) {
+        this.days = days;
+        this.costs = costs;
+        memo = new Integer[days.length];
+        return dp(0);
+    }
+
+    public int dp(int i) {
+        if (i >= days.length) {
+            return 0;
+        }
+        if (memo[i] != null) {
+            return memo[i];
+        }
+        memo[i] = Integer.MAX_VALUE;
+        int j = i;
+        for (int k = 0; k < 3; ++k) {
+            while (j < days.length && days[j] < days[i] + durations[k]) {
+                j++;
+            }
+            memo[i] = Math.min(memo[i], dp(j) + costs[k]);
+        }
+        return memo[i];
+    }
+}
