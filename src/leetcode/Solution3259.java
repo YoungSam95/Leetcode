e512e7f55b8f26e41bee7a120402d55b030d3f9c
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-11-01 00:03:49 +0800

    3259. 超级饮料的最大强化能量

---
+++ src/leetcode/Solution3259.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 3259. 超级饮料的最大强化能量
+ */
+public class Solution3259 {
+    public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
+        int n = energyDrinkA.length;
+        long[][] d = new long[n + 1][2];
+        for (int i = 1; i <= n; i++) {
+            d[i][0] = d[i - 1][0] + energyDrinkA[i - 1];
+            d[i][1] = d[i - 1][1] + energyDrinkB[i - 1];
+            if (i >= 2) {
+                d[i][0] = Math.max(d[i][0], d[i - 2][1] + energyDrinkA[i - 1]);
+                d[i][1] = Math.max(d[i][1], d[i - 2][0] + energyDrinkB[i - 1]);
+            }
+        }
+        return Math.max(d[n][0], d[n][1]);
+    }
+}
