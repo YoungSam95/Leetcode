7e4ef3c2ff768811f4175ab4a0b19ae97ab5a485
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-11-02 00:03:47 +0800

    3226. 使两个整数相等的位更改次数

---
+++ src/leetcode/Solution3226.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 3226. 使两个整数相等的位更改次数
+ */
+public class Solution3226 {
+    public int minChanges(int n, int k) {
+        int res = 0;
+        while (n > 0 || k > 0) {
+            if ((n & 1) == 0 && (k & 1) == 1) {
+                return -1;
+            }
+            if ((n & 1) == 1 && (k & 1) == 0) {
+                res++;
+            }
+            n >>= 1;
+            k >>= 1;
+        }
+        return res;
+    }
+}
