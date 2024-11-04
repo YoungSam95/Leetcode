5e5e034ba868090a906d306a29f87da7487bccca
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-11-04 21:55:42 +0800

    633. 平方数之和

---
+++ src/leetcode/Solution633.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 633. 平方数之和
+ */
+public class Solution633 {
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
