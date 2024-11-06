408cdc0b75cd7c89a1caba9aff238781617776e3
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-11-07 00:03:42 +0800

    3255. 长度为 K 的子数组的能量值 II

---
+++ src/leetcode/Solution3255.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 3255. 长度为 K 的子数组的能量值 II
+ */
+public class Solution3255 {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Arrays.fill(ans, -1);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt = i == 0 || nums[i] - nums[i - 1] != 1 ? 1 : cnt + 1;
+            if (cnt >= k) {
+                ans[i - k + 1] = nums[i];
+            }
+        }
+        return ans;
+    }
+}
