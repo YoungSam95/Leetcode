3aa922efab05199835ab1e2cce340776f53f247a
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-11-23 00:03:45 +0800

    3238. 求出胜利玩家的数目

---
+++ src/leetcode/Solution3238.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 3238. 求出胜利玩家的数目
+ */
+public class Solution3238 {
+    public int winningPlayerCount(int n, int[][] pick) {
+        int[][] cnt = new int[n][11];
+        for (int[] p : pick) {
+            cnt[p[0]][p[1]]++;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= 10; j++) {
+                if (cnt[i][j] > i) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
