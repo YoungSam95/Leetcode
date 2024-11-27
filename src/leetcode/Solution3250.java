d2f3409186a84529fc9d8f70ee665fbf2cb0f8e7
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2024-11-28 00:05:15 +0800

    3250. 单调数组对的数目 I

---
+++ src/leetcode/Solution3250.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 3250. 单调数组对的数目 I
+ */
+public class Solution3250 {
+    public int countOfPairs(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][51];
+        int mod = 1000000007;
+        for (int v = 0; v <= nums[0]; v++) {
+            dp[0][v] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int v2 = 0; v2 <= nums[i]; v2++) {
+                for (int v1 = 0; v1 <= v2; v1++) {
+                    if (nums[i - 1] - v1 >= nums[i] - v2 && nums[i] - v2 >= 0) {
+                        dp[i][v2] = (dp[i][v2] + dp[i - 1][v1]) % mod;
+                    }
+                }
+            }
+        }
+
+        int res = 0;
+        for (int v : dp[n - 1]) {
+            res = (res + v) % mod;
+        }
+        return res;
+    }
+}
