4192de02c31e2ffedd37705cd9ae2d075ca95194
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-01-27 12:26:29 +0800

    45. 跳跃游戏 II

---
+++ src/leetcode/Solution45.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 45. 跳跃游戏 II
+ */
+public class Solution45 {
+    public int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+}
