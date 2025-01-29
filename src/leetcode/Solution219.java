7ebce7a3a11529d4027e37a2674d9e3d0bed8316
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-01-29 08:52:22 +0800

    219. 存在重复元素 II

---
+++ src/leetcode/Solution219.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 219. 存在重复元素 II
+ */
+public class Solution219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+}
