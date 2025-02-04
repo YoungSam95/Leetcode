7f88bd4a83a4b0c5c00823a284988d14b92a0e1d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-02-04 08:45:55 +0800

    922. 按奇偶排序数组 II

---
+++ src/leetcode/Solution922.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * 922. 按奇偶排序数组 II
+ */
+public class Solution922 {
+    public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int i = 0;
+        for (int x : nums) {
+            if (x % 2 == 0) {
+                ans[i] = x;
+                i += 2;
+            }
+        }
+        i = 1;
+        for (int x : nums) {
+            if (x % 2 == 1) {
+                ans[i] = x;
+                i += 2;
+            }
+        }
+        return ans;
+    }
+}
