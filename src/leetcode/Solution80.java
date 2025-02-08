f3efac0cea4e2c938d57419ff44729e1683ed333
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-02-09 00:03:06 +0800

    80. 删除有序数组中的重复项 II

---
+++ src/leetcode/Solution80.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * 80. 删除有序数组中的重复项 II
+ */
+public class Solution80 {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+}
