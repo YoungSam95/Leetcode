682715ebc3608a08b5d151850328b0d8dd18febb
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-04-03 00:05:26 +0800

    2874. 有序三元组中的最大值 II

---
+++ src/leetcode/Solution2874.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * 2874. 有序三元组中的最大值 II
+ */
+public class Solution2874 {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        long res = 0, imax = 0, dmax = 0;
+        for (int k = 0; k < n; k++) {
+            res = Math.max(res, dmax * nums[k]);
+            dmax = Math.max(dmax, imax - nums[k]);
+            imax = Math.max(imax, nums[k]);
+        }
+        return res;
+    }
+}
