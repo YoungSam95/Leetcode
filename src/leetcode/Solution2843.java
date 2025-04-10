a38cea73610299694525ca98baea5c416ca060b9
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-04-11 00:06:33 +0800

    2843. 统计对称整数的数目

---
+++ src/leetcode/Solution2843.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 2843. 统计对称整数的数目
+ */
+public class Solution2843 {
+    public int countSymmetricIntegers(int low, int high) {
+        int res = 0;
+        for (int a = low; a <= high; ++a) {
+            if (a < 100 && a % 11 == 0) {
+                res++;
+            } else if (1000 <= a && a < 10000) {
+                int left = a / 1000 + (a % 1000) / 100;
+                int right = (a % 100) / 10 + a % 10;
+                if (left == right) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+}
