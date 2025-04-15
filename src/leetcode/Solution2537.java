709c2f704201d586d9210a2a8a9be66000445b19
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-04-16 00:10:25 +0800

    2537. 统计好子数组的数目

---
+++ src/leetcode/Solution2537.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/**
+ * 2537. 统计好子数组的数目
+ */
+public class Solution2537 {
+    public long countGood(int[] nums, int k) {
+        int n = nums.length;
+        int same = 0, right = -1;
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        long ans = 0;
+        for (int left = 0; left < n; ++left) {
+            while (same < k && right + 1 < n) {
+                ++right;
+                same += cnt.getOrDefault(nums[right], 0);
+                cnt.put(nums[right], cnt.getOrDefault(nums[right], 0) + 1);
+            }
+            if (same >= k) {
+                ans += n - right;
+            }
+            cnt.put(nums[left], cnt.get(nums[left]) - 1);
+            same -= cnt.get(nums[left]);
+        }
+        return ans;
+    }
+}
