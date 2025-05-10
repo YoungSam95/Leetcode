9ed488e828b78b3c8a65689581730bd2e547c0d1
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-05-11 00:04:25 +0800

    1550. 存在连续三个奇数的数组

---
+++ src/leetcode/Solution1550.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 1550. 存在连续三个奇数的数组
+ */
+public class Solution1550 {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i <= n - 3; ++i) {
+            if ((arr[i] & 1) != 0 && (arr[i + 1] & 1) != 0 && (arr[i + 2] & 1) != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
