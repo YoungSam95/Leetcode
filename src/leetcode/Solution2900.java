55d10cc204bb5f41042730d2299083b16691e41c
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-05-15 00:03:08 +0800

    2900. 最长相邻不相等子序列 I

---
+++ src/leetcode/Solution2900.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2900. 最长相邻不相等子序列 I
+ */
+public class Solution2900 {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> ans = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                ans.add(words[i]);
+            }
+        }
+        return ans;
+    }
+}
