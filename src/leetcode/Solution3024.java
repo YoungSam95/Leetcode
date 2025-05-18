5d0578fc6448060d13c9d5c4197e649794577670
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-05-19 00:10:03 +0800

    3024. 三角形类型

---
+++ src/leetcode/Solution3024.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 3024. 三角形类型
+ */
+public class Solution3024 {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2]) {
+            return "none";
+        } else if (nums[0] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
