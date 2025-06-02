4dfb6e573cb99ee894e3320361f671be1211b3aa
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-06-03 00:06:01 +0800

    1298. 你能从盒子里获得的最大糖果数

---
+++ src/leetcode/Solution1298.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 1298. 你能从盒子里获得的最大糖果数
+ */
+public class Solution1298 {
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        int n = status.length;
+        boolean[] canOpen = new boolean[n];
+        boolean[] hasBox = new boolean[n];
+        boolean[] used = new boolean[n];
+
+        for (int i = 0; i < n; ++i) {
+            canOpen[i] = (status[i] == 1);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        int ans = 0;
+        for (int box : initialBoxes) {
+            hasBox[box] = true;
+            if (canOpen[box]) {
+                q.offer(box);
+                used[box] = true;
+                ans += candies[box];
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int bigBox = q.poll();
+            for (int key : keys[bigBox]) {
+                canOpen[key] = true;
+                if (!used[key] && hasBox[key]) {
+                    q.offer(key);
+                    used[key] = true;
+                    ans += candies[key];
+                }
+            }
+            for (int box : containedBoxes[bigBox]) {
+                hasBox[box] = true;
+                if (!used[box] && canOpen[box]) {
+                    q.offer(box);
+                    used[box] = true;
+                    ans += candies[box];
+                }
+            }
+        }
+
+        return ans;
+    }
+}
