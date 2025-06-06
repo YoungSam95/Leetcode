4e73812a205464be43e9bd33d52351374fb0094f
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-06-07 00:05:59 +0800

    3170. 删除星号以后字典序最小的字符串

---
+++ src/leetcode/Solution3170.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 3170. 删除星号以后字典序最小的字符串
+ */
+public class Solution3170 {
+    public String clearStars(String s) {
+        Deque<Integer>[] cnt = new Deque[26];
+        for (int i = 0; i < 26; i++) {
+            cnt[i] = new ArrayDeque<>();
+        }
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != '*') {
+                cnt[arr[i] - 'a'].push(i);
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    if (!cnt[j].isEmpty()) {
+                        arr[cnt[j].pop()] = '*';
+                        break;
+                    }
+                }
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (char c : arr) {
+            if (c != '*') {
+                ans.append(c);
+            }
+        }
+        return ans.toString();
+    }
+}
