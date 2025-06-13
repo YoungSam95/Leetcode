f261b0ae9e5529dbe4166a668901efc897f4463b
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-06-14 00:05:25 +0800

    2566. 替换一个数字后的最大差值

---
+++ src/leetcode/Solution2566.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 2566. 替换一个数字后的最大差值
+ */
+public class Solution2566 {
+    public int minMaxDifference(int num) {
+        String s = Integer.toString(num);
+        String t = s;
+        int pos = 0;
+        while (pos < s.length() && s.charAt(pos) == '9') {
+            pos++;
+        }
+        if (pos < s.length()) {
+            s = s.replace(s.charAt(pos), '9');
+        }
+        t = t.replace(t.charAt(0), '0');
+        return Integer.parseInt(s) - Integer.parseInt(t);
+    }
+}
