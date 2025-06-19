1378936daab5dc0c4906a81d39bbd62b2d913343
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-06-20 00:05:40 +0800

    3443. K 次修改后的最大曼哈顿距离

---
+++ src/leetcode/Solution3443.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * 3443. K 次修改后的最大曼哈顿距离
+ */
+public class Solution3443 {
+    public int maxDistance(String s, int k) {
+        int latitude = 0, longitude = 0, ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'N':
+                    latitude++;
+                    break;
+                case 'S':
+                    latitude--;
+                    break;
+                case 'E':
+                    longitude++;
+                    break;
+                case 'W':
+                    longitude--;
+                    break;
+            }
+            ans = Math.max(ans, Math.min(Math.abs(latitude) + Math.abs(longitude) + k * 2, i + 1));
+        }
+        return ans;
+    }
+}
