f9a25dd4e4df8498a973caa20e0066ec52976f94
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-07-05 00:08:34 +0800

    1394. 找出数组中的幸运数

---
+++ src/leetcode/Solution1394.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1394. 找出数组中的幸运数
+ */
+public class Solution1394 {
+    public int findLucky(int[] arr) {
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+        for (int x : arr) {
+            m.put(x, m.getOrDefault(x, 0) + 1);
+        }
+        int ans = -1;
+        for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+            int key = entry.getKey(), value = entry.getValue();
+            if (key == value) {
+                ans = Math.max(ans, key);
+            }
+        }
+        return ans;
+    }
+}
