4b32c75f0a2a5e6c5800e94533e4d8e47f41906a
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-07-09 00:04:32 +0800

    3439. 重新安排会议得到最多空余时间 I

---
+++ src/leetcode/Solution3439.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 3439. 重新安排会议得到最多空余时间 I
+ */
+public class Solution3439 {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int n = startTime.length, res = 0;
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + endTime[i] - startTime[i];
+        }
+        for (int i = k - 1; i < n; i++) {
+            int right = i == n - 1 ? eventTime : startTime[i + 1];
+            int left = i == k - 1 ? 0 : endTime[i - k];
+            res = Math.max(res, right - left - (sum[i + 1] - sum[i - k + 1]));
+        }
+        return res;
+    }
+}
