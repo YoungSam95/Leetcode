b619e2b19b4be652f00085cccb159b8c6ea22ae0
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-07-28 00:04:33 +0800

    2044. 统计按位或能得到最大值的子集数目

---
+++ src/leetcode/Solution2044.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 2044. 统计按位或能得到最大值的子集数目
+ */
+public class Solution2044 {
+    int[] nums;
+    int maxOr, cnt;
+
+    public int countMaxOrSubsets(int[] nums) {
+        this.nums = nums;
+        this.maxOr = 0;
+        this.cnt = 0;
+        dfs(0, 0);
+        return cnt;
+    }
+
+    public void dfs(int pos, int orVal) {
+        if (pos == nums.length) {
+            if (orVal > maxOr) {
+                maxOr = orVal;
+                cnt = 1;
+            } else if (orVal == maxOr) {
+                cnt++;
+            }
+            return;
+        }
+        dfs(pos + 1, orVal | nums[pos]);
+        dfs(pos + 1, orVal);
+    }
+}
