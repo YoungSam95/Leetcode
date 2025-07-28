d7c77fe193f8b1b7f7bd6661c55fc84f38a079fe
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-07-29 00:04:13 +0800

    2411. 按位或最大的最小子数组长度

---
+++ src/leetcode/Solution2411.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2411. 按位或最大的最小子数组长度
+ */
+public class Solution2411 {
+    public int[] smallestSubarrays(int[] nums) {
+        int n = nums.length;
+        int[] pos = new int[31];
+        Arrays.fill(pos, -1);
+        int[] ans = new int[n];
+        for (int i = n - 1; i >= 0; --i) {
+            int j = i;
+            for (int bit = 0; bit < 31; ++bit) {
+                if ((nums[i] & (1 << bit)) == 0) {
+                    if (pos[bit] != -1) {
+                        j = Math.max(j, pos[bit]);
+                    }
+                } else {
+                    pos[bit] = i;
+                }
+            }
+            ans[i] = j - i + 1;
+        }
+        return ans;
+    }
+}
