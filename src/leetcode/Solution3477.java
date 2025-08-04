1eeb178e7e494c7731bba844dfccd8c2e98d387d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-08-05 00:06:55 +0800

    3477. 水果成篮 II

---
+++ src/leetcode/Solution3477.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 3477. 水果成篮 II
+ */
+public class Solution3477 {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int count = 0;
+        int n = baskets.length;
+        for (int fruit : fruits) {
+            int unset = 1;
+            for (int i = 0; i < n; i++) {
+                if (fruit <= baskets[i]) {
+                    baskets[i] = 0;
+                    unset = 0;
+                    break;
+                }
+            }
+            count += unset;
+        }
+        return count;
+    }
+}
