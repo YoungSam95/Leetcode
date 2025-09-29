81b9291ed830788a3b0bde0f68b7756e6dbe88f6
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-09-30 00:06:27 +0800

    2221. 数组的三角和

---
+++ src/leetcode/Solution2221.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 2221. 数组的三角和
+ */
+public class Solution2221 {
+    public int triangularSum(int[] nums) {
+        List<Integer> current = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        while (current.size() > 1) {
+            List<Integer> newNums = new ArrayList<>();
+            for (int i = 0; i < current.size() - 1; ++i) {
+                newNums.add((current.get(i) + current.get(i + 1)) % 10);
+            }
+            current = newNums;
+        }
+        return current.get(0);
+    }
+}
