67d3651ba305010e426a79f6628e61795b21299b
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-10-02 00:05:15 +0800

    3100. 换水问题 II

---
+++ src/leetcode/Solution3100.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * 3100. 换水问题 II
+ */
+public class Solution3100 {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int t = 0;
+        int empty = t * numExchange + t * (t - 1) / 2;
+        int total = numBottles + t;
+        int a = 1;
+        int b = 2 * numExchange - 3;
+        int c = -2 * numBottles;
+        t = (int) Math.ceil(((-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a)));
+        return numBottles + t - 1;
+    }
+}
