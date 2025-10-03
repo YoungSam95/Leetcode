2db0d1b3b0db3c4886321fb2321fa23cfe8ce28d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-10-04 00:04:16 +0800

    11. 盛最多水的容器

---
+++ src/leetcode/Solution11.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * 11. 盛最多水的容器
+ */
+public class Solution11 {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
+    }
+}
