441ecc50a9b4e1af43dece0da0431f0624bf42de
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-11-10 00:05:55 +0800

    3542. 将所有元素变为 0 的最少操作次数

---
+++ src/leetcode/Solution3542.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 3542. 将所有元素变为 0 的最少操作次数
+ */
+public class Solution3542 {
+    public int minOperations(int[] nums) {
+        List<Integer> s = new ArrayList<>();
+        int res = 0;
+        for (int a : nums) {
+            while (!s.isEmpty() && s.get(s.size() - 1) > a) {
+                s.remove(s.size() - 1);
+            }
+            if (a == 0) continue;
+            if (s.isEmpty() || s.get(s.size() - 1) < a) {
+                res++;
+                s.add(a);
+            }
+        }
+        return res;
+    }
+}
