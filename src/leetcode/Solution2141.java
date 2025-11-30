1d5ad734ef1d77eefdadbf98a2a3291f5d56e231
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-12-01 00:07:13 +0800

    2141. 同时运行 N 台电脑的最长时间

---
+++ src/leetcode/Solution2141.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * 2141. 同时运行 N 台电脑的最长时间
+ */
+public class Solution2141 {
+    public long maxRunTime(int n, int[] batteries) {
+        long sum = 0;
+        for (int cap : batteries) {
+            sum += cap;
+        }
+
+        long left = 0, right = sum / n, ans = 0;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long total = 0;
+            for (int cap : batteries) {
+                total += Math.min(cap, mid);
+            }
+            if (total >= n * mid) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
