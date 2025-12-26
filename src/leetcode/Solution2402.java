ce3ab4846c2660c08cc25afd993e9a1232f323e2
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2025-12-27 00:04:51 +0800

    2402. 会议室 III

---
+++ src/leetcode/Solution2402.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 2402. 会议室 III
+ */
+public class Solution2402 {
+    public int mostBooked(int n, int[][] meetings) {
+        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<Integer> availRooms = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            availRooms.offer(i);
+        }
+        PriorityQueue<long[]> usedRooms = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
+        int[] usedCount = new int[n];
+        long curTime = 0;
+        for (int[] meeting : meetings) {
+            curTime = Math.max(curTime, meeting[0]);
+            while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= curTime) {
+                availRooms.offer((int)usedRooms.poll()[1]);
+            }
+            if (availRooms.isEmpty()) {
+                curTime = usedRooms.peek()[0];
+                while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= curTime) {
+                    availRooms.offer((int)usedRooms.poll()[1]);
+                }
+            }
+            int room = availRooms.poll();
+            usedCount[room]++;
+            usedRooms.offer(new long[]{curTime + meeting[1] - meeting[0], room});
+        }
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (usedCount[i] > usedCount[ans]) ans = i;
+        }
+        return ans;
+    }
+}
