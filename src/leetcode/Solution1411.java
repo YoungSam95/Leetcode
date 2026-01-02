936f11f6cdb02c8b638f5a47f9f33e0de6d9c4c7
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-01-03 00:04:06 +0800

    1411. 给 N x 3 网格图涂色的方案数

---
+++ src/leetcode/Solution1411.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 1411. 给 N x 3 网格图涂色的方案数
+ */
+public class Solution1411 {
+    static final int MOD = 1000000007;
+
+    public int numOfWays(int n) {
+        long fi0 = 6, fi1 = 6;
+        for (int i = 2; i <= n; ++i) {
+            long newFi0 = (2 * fi0 + 2 * fi1) % MOD;
+            long newFi1 = (2 * fi0 + 3 * fi1) % MOD;
+            fi0 = newFi0;
+            fi1 = newFi1;
+        }
+        return (int) ((fi0 + fi1) % MOD);
+    }
+}
