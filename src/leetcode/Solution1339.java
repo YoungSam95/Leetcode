a2af52301002be3ea4612631046b7a6ad947791a
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-01-07 00:04:32 +0800

    1339. 分裂二叉树的最大乘积

---
+++ src/leetcode/Solution1339.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import common.TreeNode;
+
+/**
+ * 1339. 分裂二叉树的最大乘积
+ */
+public class Solution1339 {
+    private int sum = 0;
+    private int best = 0;
+
+    public int maxProduct(TreeNode root) {
+        dfs(root);
+        dfs2(root);
+        return (int)((long)best * (sum - best) % 1000000007);
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        sum += node.val;
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+    private int dfs2(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int cur = dfs2(node.left) + dfs2(node.right) + node.val;
+        if (Math.abs(cur * 2 - sum) < Math.abs(best * 2 - sum)) {
+            best = cur;
+        }
+        return cur;
+    }
+}
