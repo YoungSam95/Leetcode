93844305df152377cdab2b887f3fe66a762190e3
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-01-08 00:05:01 +0800

    1458. 两个子序列的最大点积

---
+++ src/leetcode/Solution1458.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * 1458. 两个子序列的最大点积
+ */
+public class Solution1458 {
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[][] f = new int[m][n];
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int xij = nums1[i] * nums2[j];
+                f[i][j] = xij;
+                if (i > 0) {
+                    f[i][j] = Math.max(f[i][j], f[i - 1][j]);
+                }
+                if (j > 0) {
+                    f[i][j] = Math.max(f[i][j], f[i][j - 1]);
+                }
+                if (i > 0 && j > 0) {
+                    f[i][j] = Math.max(f[i][j], f[i - 1][j - 1] + xij);
+                }
+            }
+        }
+
+        return f[m - 1][n - 1];
+    }
+}
