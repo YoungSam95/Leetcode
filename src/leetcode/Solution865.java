46b7df5e3c3266783632f290863af7c02e80a004
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-01-09 00:04:30 +0800

    865. 具有所有最深节点的最小子树

---
+++ src/leetcode/Solution865.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import common.TreeNode;
+import javafx.util.Pair;
+
+/**
+ * 865. 具有所有最深节点的最小子树
+ */
+public class Solution865 {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return f(root).getKey();
+    }
+
+    private Pair<TreeNode, Integer> f(TreeNode root) {
+        if (root == null) {
+            return new Pair<>(root, 0);
+        }
+
+        Pair<TreeNode, Integer> left = f(root.left);
+        Pair<TreeNode, Integer> right = f(root.right);
+
+        if (left.getValue() > right.getValue()) {
+            return new Pair<>(left.getKey(), left.getValue() + 1);
+        }
+        if (left.getValue() < right.getValue()) {
+            return new Pair<>(right.getKey(), right.getValue() + 1);
+        }
+        return new Pair<>(root, left.getValue() + 1);
+    }
+}
