e644b3662f287d51ca221408cd02bae5916f328e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-01-15 00:05:15 +0800

    2943. 最大化网格图中正方形空洞的面积

---
+++ src/leetcode/Solution2943.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2943. 最大化网格图中正方形空洞的面积
+ */
+public class Solution2943 {
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        Arrays.sort(hBars);
+        Arrays.sort(vBars);
+        int hmax = 1, vmax = 1;
+        int hcur = 1, vcur = 1;
+        for (int i = 1; i < hBars.length; i++) {
+            if (hBars[i] == hBars[i - 1] + 1) {
+                hcur++;
+            } else {
+                hcur = 1;
+            }
+            hmax = Math.max(hmax, hcur);
+        }
+        for (int i = 1; i < vBars.length; i++) {
+            if (vBars[i] == vBars[i - 1] + 1) {
+                vcur++;
+            } else {
+                vcur = 1;
+            }
+            vmax = Math.max(vmax, vcur);
+        }
+        int side = Math.min(hmax, vmax) + 1;
+        return side * side;
+    }
+}
