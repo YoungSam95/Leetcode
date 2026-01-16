0bccf2246a3f48ee0cc560dc8f86ab3f34d0b61d
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-01-17 00:07:02 +0800

    3047. 求交集区域内的最大正方形面积

---
+++ src/leetcode/Solution3047.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * 3047. 求交集区域内的最大正方形面积
+ */
+public class Solution3047 {
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n = bottomLeft.length;
+        long maxSide = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int w = Math.min(topRight[i][0], topRight[j][0]) -
+                        Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                int h = Math.min(topRight[i][1], topRight[j][1]) -
+                        Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                int side = Math.min(w, h);
+
+                maxSide = Math.max(maxSide, side);
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+}
