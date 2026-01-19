8945233df88297475f23df248eedd5b4a2289bbb
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-01-20 00:03:45 +0800

    3314. 构造最小位运算数组 I

---
+++ src/leetcode/Solution3314.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * 3314. 构造最小位运算数组 I
+ */
+public class Solution3314 {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = nums.get(i);
+            int res = -1;
+            int d = 1;
+            while ((x & d) != 0) {
+                res = x - d;
+                d <<= 1;
+            }
+            result[i] = res;
+        }
+        return result;
+    }
+}
