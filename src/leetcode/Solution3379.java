e417b4d0a50b9008a63460d09cac1918f6ea1299
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-02-05 00:04:57 +0800

    3379. 转换数组

---
+++ src/leetcode/Solution3379.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 3379. 转换数组
+ */
+public class Solution3379 {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = nums[((i + nums[i]) % n + n) % n];
+        }
+        return res;
+    }
+
+}
