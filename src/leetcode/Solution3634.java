c03aaeee5b5bb5a3c752490e31ea88524c407087
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-02-06 00:06:39 +0800

    3634. 使数组平衡的最少移除数目

---
+++ src/leetcode/Solution3634.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 3634. 使数组平衡的最少移除数目
+ */
+public class Solution3634 {
+    public int minRemoval(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int ans = n;
+        int right = 0;
+
+        for (int left = 0; left < n; left++) {
+            while (right < n && nums[right] <= (long)nums[left] * k) {
+                right++;
+            }
+            ans = Math.min(ans, n - (right - left));
+        }
+
+        return ans;
+    }
+}
