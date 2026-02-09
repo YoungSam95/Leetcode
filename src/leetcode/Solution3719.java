c19da658b16324b764a6e0ae142205191a1d33d4
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-02-10 00:07:22 +0800

    3719. 最长平衡子数组 I

---
+++ src/leetcode/Solution3719.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/**
+ * 3719. 最长平衡子数组 I
+ */
+public class Solution3719 {
+    public int longestBalanced(int[] nums) {
+        int len = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            HashMap<Integer, Integer> odd = new HashMap<>();
+            HashMap<Integer, Integer> even = new HashMap<>();
+
+            for (int j = i; j < nums.length; j++) {
+                HashMap<Integer, Integer> map = (nums[j] & 1) == 1 ? odd : even;
+                map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+                if (odd.size() == even.size()) {
+                    len = Math.max(len, j - i + 1);
+                }
+            }
+        }
+
+        return len;
+    }
+}
