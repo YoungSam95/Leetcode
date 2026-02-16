648916d52c974da7b9103cfd06ec19b860dda9d2
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-02-16 11:10:13 +0800

    190. 颠倒二进制位

---
+++ src/leetcode/Solution190.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * 190. 颠倒二进制位
+ */
+public class Solution190 {
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
