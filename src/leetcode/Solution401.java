a67e2b2bce527fe348b5beaee218a49abbc07287
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-02-17 12:36:57 +0800

    401. 二进制手表

---
+++ src/leetcode/Solution401.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 401. 二进制手表
+ */
+public class Solution401 {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<String>();
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    ans.add(h + ":" + (m < 10 ? "0" : "") + m);
+                }
+            }
+        }
+        return ans;
+    }
+}
