6a6ed1c2723d68340a8ced731d8b07912df86509
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-02-19 10:30:57 +0800

    696. 计数二进制子串

---
+++ src/leetcode/Solution696.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 696. 计数二进制子串
+ */
+public class Solution696 {
+    public int countBinarySubstrings(String s) {
+        int ptr = 0, n = s.length(), last = 0, ans = 0;
+        while (ptr < n) {
+            char c = s.charAt(ptr);
+            int count = 0;
+            while (ptr < n && s.charAt(ptr) == c) {
+                ++ptr;
+                ++count;
+            }
+            ans += Math.min(count, last);
+            last = count;
+        }
+        return ans;
+    }
+}
