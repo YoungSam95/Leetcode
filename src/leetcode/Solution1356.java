0fabbd8404509383319dae28328acdb77bafca93
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-02-25 22:32:07 +0800

    1356. 根据数字二进制下 1 的数目排序

---
+++ src/leetcode/Solution1356.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 1356. 根据数字二进制下 1 的数目排序
+ */
+public class Solution1356 {
+    public int[] sortByBits(int[] arr) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int x : arr) {
+            list.add(x);
+        }
+        int[] bit = new int[10001];
+        for (int i = 1; i <= 10000; ++i) {
+            bit[i] = bit[i >> 1] + (i & 1);
+        }
+        Collections.sort(list, new Comparator<Integer>() {
+            public int compare(Integer x, Integer y) {
+                if (bit[x] != bit[y]) {
+                    return bit[x] - bit[y];
+                } else {
+                    return x - y;
+                }
+            }
+        });
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
