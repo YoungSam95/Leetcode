fcd72ed3181e9873877de9721a6eda69bd4295cb
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-03-11 00:06:35 +0800

    1009. 十进制整数的反码

---
+++ src/leetcode/Solution1009.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 1009. 十进制整数的反码
+ */
+public class Solution1009 {
+    public int bitwiseComplement(int n) {
+        int highbit = 0;
+        for (int i = 1; i <= 30; ++i) {
+            if (n >= 1 << i) {
+                highbit = i;
+            } else {
+                break;
+            }
+        }
+        int mask = highbit == 30 ? 0x7fffffff : (1 << (highbit + 1)) - 1;
+        return n ^ mask;
+    }
+}
