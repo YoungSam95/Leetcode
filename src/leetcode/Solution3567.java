628ec3a92eac610bbc77ffb8872cb29374366c90
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-03-20 00:11:44 +0800

    3567. 子矩阵的最小绝对差

---
+++ src/leetcode/Solution3567.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 3567. 子矩阵的最小绝对差
+ */
+public class Solution3567 {
+    public int[][] minAbsDiff(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        int[][] res = new int[m - k + 1][n - k + 1];
+        for (int i = 0; i + k <= m; i++) {
+            for (int j = 0; j + k <= n; j++) {
+                List<Integer> kgrid = new ArrayList<>();
+                for (int x = i; x < i + k; x++) {
+                    for (int y = j; y < j + k; y++) {
+                        kgrid.add(grid[x][y]);
+                    }
+                }
+                int kmin = Integer.MAX_VALUE;
+                Collections.sort(kgrid);
+                for (int t = 1; t < kgrid.size(); t++) {
+                    if (kgrid.get(t).equals(kgrid.get(t - 1))) {
+                        continue;
+                    }
+                    kmin = Math.min(kmin, kgrid.get(t) - kgrid.get(t - 1));
+                }
+                if (kmin != Integer.MAX_VALUE) {
+                    res[i][j] = kmin;
+                }
+            }
+        }
+        return res;
+    }
+}
