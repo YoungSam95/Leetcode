139fd5c0d96e5ea1e053b6135b38f562e45358a1
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-03-27 00:04:22 +0800

    2946. 循环移位后的矩阵相似检查

---
+++ src/leetcode/Solution2946.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 2946. 循环移位后的矩阵相似检查
+ */
+public class Solution2946 {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        k %= n;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != mat[i][(j + k) % n]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
