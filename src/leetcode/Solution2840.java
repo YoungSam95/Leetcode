b46b6e444947a907e6058f6fb254cbce03951acc
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-03-30 00:05:43 +0800

    2840. 判断通过操作能否让字符串相等 II

---
+++ src/leetcode/Solution2840.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2840. 判断通过操作能否让字符串相等 II
+ */
+public class Solution2840 {
+    public boolean checkStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[256];
+        int[] count2 = new int[256];
+
+        for (int i = 0; i < s1.length(); i++) {
+            int offset = (i & 1) << 7;
+            count1[offset + s1.charAt(i)]++;
+            count2[offset + s2.charAt(i)]++;
+        }
+
+        return Arrays.equals(count1, count2);
+    }
+}
