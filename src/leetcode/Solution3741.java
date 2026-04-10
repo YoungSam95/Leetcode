a80c34998f39282e860f38db94650bfe9bd4b25e
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-04-11 00:09:31 +0800

    3741. 三个相等元素之间的最小距离 II

---
+++ src/leetcode/Solution3741.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 3741. 三个相等元素之间的最小距离 II
+ */
+public class Solution3741 {
+    public int minimumDistance(int[] nums) {
+        int n = nums.length;
+        int[] next = new int[n];
+        Arrays.fill(next, -1);
+        Map<Integer, Integer> occur = new HashMap<>();
+        int ans = n + 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (occur.containsKey(nums[i])) {
+                next[i] = occur.get(nums[i]);
+            }
+            occur.put(nums[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int secondPos = next[i];
+            if (secondPos != -1) {
+                int thirdPos = next[secondPos];
+                if (thirdPos != -1) {
+                    ans = Math.min(ans, thirdPos - i);
+                }
+            }
+        }
+
+        return ans == n + 1 ? -1 : ans * 2;
+    }
+}
