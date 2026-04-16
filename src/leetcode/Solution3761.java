93cfa8f326b6cd55d73b206c1c5a3185cb3d9e46
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-04-17 00:05:01 +0800

    3761. 镜像对之间最小绝对距离

---
+++ src/leetcode/Solution3761.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 3761. 镜像对之间最小绝对距离
+ */
+public class Solution3761 {
+    public int minMirrorPairDistance(int[] nums) {
+        Map<Integer, Integer> prev = new HashMap<>();
+        int n = nums.length;
+        int ans = n + 1;
+
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            if (prev.containsKey(x)) {
+                ans = Math.min(ans, i - prev.get(x));
+            }
+            prev.put(reverseNum(x), i);
+        }
+
+        return ans == n + 1 ? -1 : ans;
+    }
+
+    private int reverseNum(int x) {
+        int y = 0;
+        while (x > 0) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        return y;
+    }
+}
