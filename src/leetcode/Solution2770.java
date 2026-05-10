66914381344ebb01b30c717eb323426e7a7420cb
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-05-10 22:14:48 +0800

    2770. 达到末尾下标所需的最大跳跃次数

---
+++ src/leetcode/Solution2770.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 2770. 达到末尾下标所需的最大跳跃次数
+ */
+public class Solution2770 {
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (Math.abs(nums[j] - nums[i]) <= target) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1] < 0 ? -1 : dp[n - 1];
+    }
+}
