360d388f9e12318b69f885f6776743f2f1acb3e2
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-06-16 00:17:08 +0800

    3612. 用特殊操作处理字符串 I

---
+++ src/leetcode/Solution3612.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 3612. 用特殊操作处理字符串 I
+ */
+public class Solution3612 {
+
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                if (result.length() > 0) result.deleteCharAt(result.length() - 1);
+            } else if (ch == '#') {
+                result.append(result.toString());
+            } else if (ch == '%') {
+                result.reverse();
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
