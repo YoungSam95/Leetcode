f8b86fe0edd27aa831d7ccb2bf89dc4e8ff4e1c0
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-06-18 00:26:04 +0800

    1344. 时钟指针的夹角

---
+++ src/leetcode/Solution1344.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * 1344. 时钟指针的夹角
+ */
+public class Solution1344 {
+    public double angleClock(int hour, int minutes) {
+        int oneMinAngle = 6;
+        int oneHourAngle = 30;
+
+        double minutesAngle = oneMinAngle * minutes;
+        double hourAngle = (hour % 12 + minutes / 60.0) * oneHourAngle;
+
+        double diff = Math.abs(hourAngle - minutesAngle);
+        return Math.min(diff, 360 - diff);
+    }
+}
