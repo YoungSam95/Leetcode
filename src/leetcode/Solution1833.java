773506b4c209638fe9cc48153c1b4d6431dd2d50
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-06-21 00:31:41 +0800

    1833. 雪糕的最大数量

---
+++ src/leetcode/Solution1833.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 1833. 雪糕的最大数量
+ */
+public class Solution1833 {
+    public int maxIceCream(int[] costs, int coins) {
+        int[] freq = new int[100001];
+        for (int cost : costs) {
+            freq[cost]++;
+        }
+        int count = 0;
+        for (int i = 1; i <= 100000; i++) {
+            if (coins >= i) {
+                int curCount = Math.min(freq[i], coins / i);
+                count += curCount;
+                coins -= i * curCount;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
