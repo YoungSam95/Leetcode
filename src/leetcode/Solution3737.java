d5a7105d66077993d93822e3541ee5e9da27add8
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-06-25 00:14:58 +0800

    3737. 统计主要元素子数组数目 I

---
+++ src/leetcode/Solution3737.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 3737. 统计主要元素子数组数目 I
+ */
+public class Solution3737 {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = i; j < n; ++j) {
+                cnt += (nums[j] == target ? 1 : -1);
+                if (cnt > 0) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
