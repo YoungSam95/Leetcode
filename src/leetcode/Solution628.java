645536fb027cd87020aea36d8b1bbd6f0581cc28
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-07-26 00:06:41 +0800

    628. 三个数的最大乘积

---
+++ src/leetcode/Solution628.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 628. 三个数的最大乘积
+ */
+public class Solution628 {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+    }
+}
