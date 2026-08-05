6109b9ea5ed69d84e7af13fe568a7e6908be1317
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-08-06 00:13:58 +0800

    3345. 最小可整除数位乘积 I

---
+++ src/leetcode/Solution3345.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * 3345. 最小可整除数位乘积 I
+ */
+public class Solution3345 {
+    public int smallestNumber(int n, int t) {
+        while (!check(n, t)) {
+            n++;
+        }
+        return n;
+    }
+
+    private boolean check(int num, int t) {
+        int product = 1;
+        while (num > 0) {
+            product *= num % 10;
+            num /= 10;
+            if (product == 0) {
+                break;
+            }
+        }
+        return product % t == 0;
+    }
+}
