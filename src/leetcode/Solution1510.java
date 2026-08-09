6df461f4b47849156ca147e3b8230ec774c74499
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-08-10 00:11:46 +0800

    1510. 石子游戏 IV

---
+++ src/leetcode/Solution1510.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 1510. 石子游戏 IV
+ */
+public class Solution1510 {
+    public boolean winnerSquareGame(int n) {
+        boolean[] f = new boolean[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            for (int k = 1; k * k <= i; ++k) {
+                if (!f[i - k * k]) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[n];
+    }
+}
