b15684648b6e326849d42f9b2a40840534558492
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-08-24 00:04:13 +0800

    1872. 石子游戏 VIII

---
+++ src/leetcode/Solution1872.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * 1872. 石子游戏 VIII
+ */
+public class Solution1872 {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        int[] pre = new int[n];
+        pre[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] + stones[i];
+        }
+
+        int[] f = new int[n];
+        f[n - 1] = pre[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            f[i] = Math.max(f[i + 1], pre[i] - f[i + 1]);
+        }
+        return f[1];
+    }
+}
