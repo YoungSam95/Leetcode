11368fbc66ead235948bb076c5513914542febc3
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2026-09-05 00:08:14 +0800

    3904. 最小稳定下标 II

---
+++ src/leetcode/Solution3904.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 3904. 最小稳定下标 II
+ */
+public class Solution3904 {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] minValue = new int[n];
+        minValue[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minValue[i] = Math.min(minValue[i + 1], nums[i]);
+        }
+
+        int maxValue = 0;
+        for (int i = 0; i < n; i++) {
+            maxValue = Math.max(maxValue, nums[i]);
+            if (maxValue - minValue[i] <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
